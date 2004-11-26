f2f9d36b59600395284312645700ec4b23a190a2
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:13:50 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:17 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:49 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:02 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:30 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:43 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/sdraw/04/makefile.mk
+++ helpcontent2/source/text/sdraw/04/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:12 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 20:13:50 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/sdraw/04
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_sdraw_04
 # edit to match the current module
 MODULE  = sdraw
@@ -32,7 +32,7 @@ MODULE  = sdraw
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    01020000.hzip 
+   01020000.hzip 
     
 # --- Targets ------------------------------------------------------
 
