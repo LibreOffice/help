78ac9e69a8ad4f41eb132e3b4abb27a2bcc5b611
Rüdiger Timm <rt@openoffice.org>
2004-11-26 20:29:36 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:20 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:52 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:06 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:34 fpe 1.3.2.2: Regular Update 2004/09/16 11:47:28 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/shared/07/makefile.mk
+++ helpcontent2/source/text/shared/07/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:15 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 21:29:36 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/shared/07
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_shared_07
 # edit to match the current module
 MODULE  = shared
@@ -32,7 +32,7 @@ MODULE  = shared
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    09000000.hzip 
+   09000000.hzip 
     
 # --- Targets ------------------------------------------------------
 
