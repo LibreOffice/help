10a165e89f26564362824feac65572275d9a44b5
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:58:37 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:12 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:45 fpe 1.3.2.4: updated makefiles 2004/10/15 09:00:58 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:27 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:31 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/scalc/guide/makefile.mk
+++ helpcontent2/source/text/scalc/guide/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:09 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 19:58:37 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/scalc/guide
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_scalc_guide
 # edit to match the current module
 MODULE  = scalc
@@ -32,84 +32,87 @@ MODULE  = scalc
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    address_auto.hzip \
-    address_byname.hzip \
-    auto_off.hzip \
-    autofilter.hzip \
-    autoformat.hzip \
-    calc_date.hzip \
-    calc_series.hzip \
-    calc_timevalues.hzip \
-    calculate.hzip \
-    cell_protect.hzip \
-    cell_unprotect.hzip \
-    cellcopy.hzip \
-    cellreference_dragdrop.hzip \
-    cellreferences.hzip \
-    cellreferences_url.hzip \
-    cellstyle_by_formula.hzip \
-    cellstyle_conditional.hzip \
-    cellstyle_minusvalue.hzip \
-    consolidate.hzip \
-    csv_files.hzip \
-    csv_formula.hzip \
-    currency_format.hzip \
-    database_define.hzip \
-    database_filter.hzip \
-    database_group.hzip \
-    database_sort.hzip \
-    datapilot_createtable.hzip \
-    datapilot_deletetable.hzip \
-    datapilot_edittable.hzip \
-    datapilot_filtertable.hzip \
-    datapilot_tipps.hzip \
-    datapilot_updatetable.hzip \
-    dbase_files.hzip \
-    design.hzip \
-    edit_multitables.hzip \
-    filters.hzip \
-    format_table.hzip \
-    format_value.hzip \
-    format_value_userdef.hzip \
-    formula_copy.hzip \
-    formula_enter.hzip \
-    formula_value.hzip \
-    formulas.hzip \
-    fraction_enter.hzip \
-    goalseek.hzip \
-    html_doc.hzip \
-    integer_leading_zero.hzip \
-    keyboard.hzip \
-    line_fix.hzip \
-    main.hzip \
-    mark_cells.hzip \
-    matrixformula.hzip \
-    multi_tables.hzip \
-    multioperation.hzip \
-    multitables.hzip \
-    note_insert.hzip \
-    print_details.hzip \
-    print_exact.hzip \
-    print_landscape.hzip \
-    print_title_row.hzip \
-    printranges.hzip \
-    relativ_absolut_ref.hzip \
-    rename_table.hzip \
-    rounding_numbers.hzip \
-    row_height.hzip \
-    sorted_list.hzip \
-    specialfilter.hzip \
-    super_subscript.hzip \
-    table_rotate.hzip \
-    table_view.hzip \
-    text_numbers.hzip \
-    text_rotate.hzip \
-    text_wrap.hzip \
-    userdefined_function.hzip \
-    validity.hzip \
-    value_with_name.hzip \
-    webquery.hzip \
-    year2000.hzip 
+   address_auto.hzip \
+   address_byname.hzip \
+   auto_off.hzip \
+   autofilter.hzip \
+   autoformat.hzip \
+   calc_date.hzip \
+   calc_series.hzip \
+   calc_timevalues.hzip \
+   calculate.hzip \
+   cell_protect.hzip \
+   cell_unprotect.hzip \
+   cellcopy.hzip \
+   cellreference_dragdrop.hzip \
+   cellreferences.hzip \
+   cellreferences_url.hzip \
+   cellstyle_by_formula.hzip \
+   cellstyle_conditional.hzip \
+   cellstyle_minusvalue.hzip \
+   consolidate.hzip \
+   csv_files.hzip \
+   csv_formula.hzip \
+   currency_format.hzip \
+   database_define.hzip \
+   database_filter.hzip \
+   database_group.hzip \
+   database_sort.hzip \
+   datapilot.hzip \
+   datapilot_createtable.hzip \
+   datapilot_deletetable.hzip \
+   datapilot_edittable.hzip \
+   datapilot_filtertable.hzip \
+   datapilot_grouping.hzip \
+   datapilot_tipps.hzip \
+   datapilot_updatetable.hzip \
+   dbase_files.hzip \
+   design.hzip \
+   edit_multitables.hzip \
+   filters.hzip \
+   format_table.hzip \
+   format_value.hzip \
+   format_value_userdef.hzip \
+   formula_copy.hzip \
+   formula_enter.hzip \
+   formula_value.hzip \
+   formulas.hzip \
+   fraction_enter.hzip \
+   goalseek.hzip \
+   html_doc.hzip \
+   integer_leading_zero.hzip \
+   keyboard.hzip \
+   line_fix.hzip \
+   main.hzip \
+   mark_cells.hzip \
+   matrixformula.hzip \
+   multi_tables.hzip \
+   multioperation.hzip \
+   multitables.hzip \
+   note_insert.hzip \
+   print_details.hzip \
+   print_exact.hzip \
+   print_landscape.hzip \
+   print_title_row.hzip \
+   printranges.hzip \
+   relativ_absolut_ref.hzip \
+   rename_table.hzip \
+   rounding_numbers.hzip \
+   row_height.hzip \
+   scenario.hzip \
+   sorted_list.hzip \
+   specialfilter.hzip \
+   super_subscript.hzip \
+   table_rotate.hzip \
+   table_view.hzip \
+   text_numbers.hzip \
+   text_rotate.hzip \
+   text_wrap.hzip \
+   userdefined_function.hzip \
+   validity.hzip \
+   value_with_name.hzip \
+   webquery.hzip \
+   year2000.hzip 
     
 # --- Targets ------------------------------------------------------
 
