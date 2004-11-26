19c2924f42e364e7af03022a3dcf5f8876303e66
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:06:15 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:13 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:46 fpe 1.3.2.4: updated makefiles 2004/10/15 09:00:58 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:27 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:35 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/schart/makefile.mk
+++ helpcontent2/source/text/schart/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:11 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 20:06:15 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/schart
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_schart
 # edit to match the current module
 MODULE  = schart
@@ -32,19 +32,19 @@ MODULE  = schart
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    main0000.hzip \
-    main0100.hzip \
-    main0101.hzip \
-    main0102.hzip \
-    main0103.hzip \
-    main0104.hzip \
-    main0105.hzip \
-    main0106.hzip \
-    main0107.hzip \
-    main0200.hzip \
-    main0202.hzip \
-    main0203.hzip \
-    main0503.hzip 
+   main0000.hzip \
+   main0100.hzip \
+   main0101.hzip \
+   main0102.hzip \
+   main0103.hzip \
+   main0104.hzip \
+   main0105.hzip \
+   main0106.hzip \
+   main0107.hzip \
+   main0200.hzip \
+   main0202.hzip \
+   main0203.hzip \
+   main0503.hzip 
     
 # --- Targets ------------------------------------------------------
 
