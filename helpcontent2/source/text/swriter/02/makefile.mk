1edc62695cbb4ab077653b1db131acd875ae649f
Rüdiger Timm <rt@openoffice.org>
2004-11-26 22:59:30 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:28 fpe 1.3.2.5: updated makefiles 2004/10/28 10:48:02 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:15 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:43 fpe 1.3.2.2: Regular Update 2004/09/16 11:48:55 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/swriter/02/makefile.mk
+++ helpcontent2/source/text/swriter/02/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:21 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 23:59:30 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/swriter/02
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_swriter_02
 # edit to match the current module
 MODULE  = swriter
@@ -32,55 +32,54 @@ MODULE  = swriter
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    02110000.hzip \
-    03210000.hzip \
-    03220000.hzip \
-    04090000.hzip \
-    04100000.hzip \
-    04210000.hzip \
-    04220000.hzip \
-    04230000.hzip \
-    04240000.hzip \
-    04250000.hzip \
-    06040000.hzip \
-    06070000.hzip \
-    06080000.hzip \
-    06090000.hzip \
-    06120000.hzip \
-    06130000.hzip \
-    06140000.hzip \
-    08010000.hzip \
-    08080000.hzip \
-    08090000.hzip \
-    10010000.hzip \
-    10020000.hzip \
-    10030000.hzip \
-    10050000.hzip \
-    10070000.hzip \
-    10080000.hzip \
-    10090000.hzip \
-    10110000.hzip \
-    14010000.hzip \
-    14020000.hzip \
-    14030000.hzip \
-    14040000.hzip \
-    14050000.hzip \
-    18010000.hzip \
-    18030000.hzip \
-    18030100.hzip \
-    18030200.hzip \
-    18030300.hzip \
-    18030400.hzip \
-    18030500.hzip \
-    18030600.hzip \
-    18030700.hzip \
-    18120000.hzip \
-    18130000.hzip \
-    19010000.hzip \
-    19020000.hzip \
-    19030000.hzip \
-    19040000.hzip \
-    19050000.hzip 
+   02110000.hzip \
+   03210000.hzip \
+   03220000.hzip \
+   04090000.hzip \
+   04100000.hzip \
+   04210000.hzip \
+   04220000.hzip \
+   04230000.hzip \
+   04240000.hzip \
+   04250000.hzip \
+   06040000.hzip \
+   06070000.hzip \
+   06080000.hzip \
+   06090000.hzip \
+   06120000.hzip \
+   06130000.hzip \
+   06140000.hzip \
+   08010000.hzip \
+   08080000.hzip \
+   10010000.hzip \
+   10020000.hzip \
+   10030000.hzip \
+   10050000.hzip \
+   10070000.hzip \
+   10080000.hzip \
+   10090000.hzip \
+   10110000.hzip \
+   14010000.hzip \
+   14020000.hzip \
+   14030000.hzip \
+   14040000.hzip \
+   14050000.hzip \
+   18010000.hzip \
+   18030000.hzip \
+   18030100.hzip \
+   18030200.hzip \
+   18030300.hzip \
+   18030400.hzip \
+   18030500.hzip \
+   18030600.hzip \
+   18030700.hzip \
+   18120000.hzip \
+   18130000.hzip \
+   19010000.hzip \
+   19020000.hzip \
+   19030000.hzip \
+   19040000.hzip \
+   19050000.hzip 
     
 # --- Targets ------------------------------------------------------
 
