1cb737496ba8f3ad3d14287c26331f529f462f08
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:20:50 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:18 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:50 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:03 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:31 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:48 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/shared/makefile.mk
+++ helpcontent2/source/text/shared/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:14 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 20:20:50 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/shared
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_shared
 # edit to match the current module
 MODULE  = shared
@@ -32,23 +32,25 @@ MODULE  = shared
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    main0108.hzip \
-    main0201.hzip \
-    main0208.hzip \
-    main0209.hzip \
-    main0212.hzip \
-    main0213.hzip \
-    main0214.hzip \
-    main0226.hzip \
-    main0227.hzip \
-    main0300.hzip \
-    main0302.hzip \
-    main0400.hzip \
-    main0500.hzip \
-    main0600.hzip \
-    main0650.hzip \
-    main0800.hzip \
-    tree_strings.hzip 
+   3dsettings_toolbar.hzip \
+   fontwork_toolbar.hzip \
+   main0108.hzip \
+   main0201.hzip \
+   main0208.hzip \
+   main0209.hzip \
+   main0212.hzip \
+   main0213.hzip \
+   main0214.hzip \
+   main0226.hzip \
+   main0227.hzip \
+   main0300.hzip \
+   main0302.hzip \
+   main0400.hzip \
+   main0500.hzip \
+   main0600.hzip \
+   main0650.hzip \
+   main0800.hzip \
+   tree_strings.hzip 
     
 # --- Targets ------------------------------------------------------
 
