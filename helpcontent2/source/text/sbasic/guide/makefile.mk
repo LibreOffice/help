edf353722d1ee005eae512f0c70ecb1f0ac9378f
Rüdiger Timm <rt@openoffice.org>
2004-11-26 13:39:47 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:08 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:41 fpe 1.3.2.4: updated makefiles 2004/10/15 09:00:54 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:23 fpe 1.3.2.2: Regular Update 2004/09/16 11:45:09 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/sbasic/guide/makefile.mk
+++ helpcontent2/source/text/sbasic/guide/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:07 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 14:39:47 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/sbasic/guide
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_sbasic_guide
 # edit to match the current module
 MODULE  = sbasic
@@ -32,11 +32,11 @@ MODULE  = sbasic
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    control_properties.hzip \
-    create_dialog.hzip \
-    insert_control.hzip \
-    sample_code.hzip \
-    show_dialog.hzip 
+   control_properties.hzip \
+   create_dialog.hzip \
+   insert_control.hzip \
+   sample_code.hzip \
+   show_dialog.hzip 
     
 # --- Targets ------------------------------------------------------
 
