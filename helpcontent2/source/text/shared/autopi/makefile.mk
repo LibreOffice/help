2bc03e5863d70aa422fc16a4681c343b39f88e18
Rüdiger Timm <rt@openoffice.org>
2004-11-26 20:46:05 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:21 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:53 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:07 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:34 fpe 1.3.2.2: Regular Update 2004/09/16 11:47:33 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/shared/autopi/makefile.mk
+++ helpcontent2/source/text/shared/autopi/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:15 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 21:46:05 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/shared/autopi
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_shared_autopi
 # edit to match the current module
 MODULE  = shared
@@ -32,53 +32,84 @@ MODULE  = shared
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    01000000.hzip \
-    01020600.hzip \
-    01020700.hzip \
-    01020800.hzip \
-    01030000.hzip \
-    01030100.hzip \
-    01030200.hzip \
-    01030300.hzip \
-    01030400.hzip \
-    01030500.hzip \
-    01050000.hzip \
-    01050100.hzip \
-    01050200.hzip \
-    01050300.hzip \
-    01050400.hzip \
-    01050500.hzip \
-    01060000.hzip \
-    01090000.hzip \
-    01100000.hzip \
-    01100100.hzip \
-    01100150.hzip \
-    01100200.hzip \
-    01100300.hzip \
-    01100400.hzip \
-    01100500.hzip \
-    01110000.hzip \
-    01110100.hzip \
-    01110200.hzip \
-    01110300.hzip \
-    01110400.hzip \
-    01110500.hzip \
-    01110600.hzip \
-    01120000.hzip \
-    01120100.hzip \
-    01120200.hzip \
-    01120300.hzip \
-    01120400.hzip \
-    01120500.hzip \
-    01130000.hzip \
-    01130100.hzip \
-    01130200.hzip \
-    01150000.hzip \
-    01170000.hzip \
-    01170200.hzip \
-    01170300.hzip \
-    01170500.hzip \
-    webwizard00.hzip 
+   01000000.hzip \
+   01010000.hzip \
+   01010100.hzip \
+   01010200.hzip \
+   01010300.hzip \
+   01010400.hzip \
+   01010500.hzip \
+   01010600.hzip \
+   01020000.hzip \
+   01020100.hzip \
+   01020200.hzip \
+   01020300.hzip \
+   01020400.hzip \
+   01020500.hzip \
+   01040000.hzip \
+   01040100.hzip \
+   01040200.hzip \
+   01040300.hzip \
+   01040400.hzip \
+   01040500.hzip \
+   01040600.hzip \
+   01050000.hzip \
+   01050100.hzip \
+   01050200.hzip \
+   01050300.hzip \
+   01050400.hzip \
+   01050500.hzip \
+   01090000.hzip \
+   01090100.hzip \
+   01090200.hzip \
+   01090210.hzip \
+   01090220.hzip \
+   01090300.hzip \
+   01090400.hzip \
+   01090500.hzip \
+   01090600.hzip \
+   01100000.hzip \
+   01100100.hzip \
+   01100150.hzip \
+   01100200.hzip \
+   01100300.hzip \
+   01100400.hzip \
+   01100500.hzip \
+   01110000.hzip \
+   01110100.hzip \
+   01110200.hzip \
+   01110300.hzip \
+   01110400.hzip \
+   01110500.hzip \
+   01110600.hzip \
+   01120000.hzip \
+   01120100.hzip \
+   01120200.hzip \
+   01120300.hzip \
+   01120400.hzip \
+   01120500.hzip \
+   01130000.hzip \
+   01130100.hzip \
+   01130200.hzip \
+   01150000.hzip \
+   01170000.hzip \
+   01170200.hzip \
+   01170300.hzip \
+   01170400.hzip \
+   01170500.hzip \
+   purchase.hzip \
+   startup.hzip \
+   webwizard00.hzip \
+   webwizard01.hzip \
+   webwizard02.hzip \
+   webwizard03.hzip \
+   webwizard04.hzip \
+   webwizard05.hzip \
+   webwizard05bi.hzip \
+   webwizard05is.hzip \
+   webwizard06.hzip \
+   webwizard07.hzip \
+   webwizard07fc.hzip 
     
 # --- Targets ------------------------------------------------------
 
