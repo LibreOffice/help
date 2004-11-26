5cbd19cd55d3574e66c11636f269db8d93d98a66
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:09:36 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:14 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:46 fpe 1.3.2.4: updated makefiles 2004/10/15 09:00:59 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:28 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:38 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/schart/01/makefile.mk
+++ helpcontent2/source/text/schart/01/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:11 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 20:09:36 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/schart/01
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_schart_01
 # edit to match the current module
 MODULE  = schart
@@ -32,36 +32,36 @@ MODULE  = schart
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    03010000.hzip \
-    04010000.hzip \
-    04020000.hzip \
-    04030000.hzip \
-    04040000.hzip \
-    04050000.hzip \
-    04060000.hzip \
-    04070000.hzip \
-    05010000.hzip \
-    05010100.hzip \
-    05010200.hzip \
-    05020000.hzip \
-    05020100.hzip \
-    05020101.hzip \
-    05020200.hzip \
-    05020201.hzip \
-    05030000.hzip \
-    05040000.hzip \
-    05040100.hzip \
-    05040200.hzip \
-    05040201.hzip \
-    05050000.hzip \
-    05050100.hzip \
-    05060000.hzip \
-    05070000.hzip \
-    05080000.hzip \
-    05090000.hzip \
-    05100000.hzip \
-    05110000.hzip \
-    05120000.hzip 
+   03010000.hzip \
+   04010000.hzip \
+   04020000.hzip \
+   04030000.hzip \
+   04040000.hzip \
+   04050000.hzip \
+   04060000.hzip \
+   04070000.hzip \
+   05010000.hzip \
+   05010100.hzip \
+   05010200.hzip \
+   05020000.hzip \
+   05020100.hzip \
+   05020101.hzip \
+   05020200.hzip \
+   05020201.hzip \
+   05030000.hzip \
+   05040000.hzip \
+   05040100.hzip \
+   05040200.hzip \
+   05040201.hzip \
+   05050000.hzip \
+   05050100.hzip \
+   05060000.hzip \
+   05070000.hzip \
+   05080000.hzip \
+   05090000.hzip \
+   05100000.hzip \
+   05110000.hzip \
+   05120000.hzip 
     
 # --- Targets ------------------------------------------------------
 
