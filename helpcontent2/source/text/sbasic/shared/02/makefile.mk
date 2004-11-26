c2fea48cb6bb8d3f5c07551bed0db0d1b93b56a3
Rüdiger Timm <rt@openoffice.org>
2004-11-26 14:48:00 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:09 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:42 fpe 1.3.2.4: updated makefiles 2004/10/15 09:00:55 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:24 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:02 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/sbasic/shared/02/makefile.mk
+++ helpcontent2/source/text/sbasic/shared/02/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:07 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 15:48:00 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/sbasic/shared/02
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_sbasic_shared_02
 # edit to match the current module
 MODULE  = sbasic
@@ -32,24 +32,24 @@ MODULE  = sbasic
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    11010000.hzip \
-    11020000.hzip \
-    11030000.hzip \
-    11040000.hzip \
-    11050000.hzip \
-    11060000.hzip \
-    11070000.hzip \
-    11080000.hzip \
-    11090000.hzip \
-    11090100.hzip \
-    11100000.hzip \
-    11110000.hzip \
-    11120000.hzip \
-    11140000.hzip \
-    11150000.hzip \
-    11160000.hzip \
-    11170000.hzip \
-    20000000.hzip 
+   11010000.hzip \
+   11020000.hzip \
+   11030000.hzip \
+   11040000.hzip \
+   11050000.hzip \
+   11060000.hzip \
+   11070000.hzip \
+   11080000.hzip \
+   11090000.hzip \
+   11090100.hzip \
+   11100000.hzip \
+   11110000.hzip \
+   11120000.hzip \
+   11140000.hzip \
+   11150000.hzip \
+   11160000.hzip \
+   11170000.hzip \
+   20000000.hzip 
     
 # --- Targets ------------------------------------------------------
 
