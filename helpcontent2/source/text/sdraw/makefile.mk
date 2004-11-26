2ebc8269471bc890a083de66adfcc79fab03fd2e
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:12:56 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:16 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:48 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:01 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:29 fpe 1.3.2.2: Regular Update 2004/09/16 11:46:41 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/sdraw/makefile.mk
+++ helpcontent2/source/text/sdraw/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:13 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 20:12:56 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/sdraw
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_sdraw
 # edit to match the current module
 MODULE  = sdraw
@@ -32,19 +32,19 @@ MODULE  = sdraw
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    main0000.hzip \
-    main0100.hzip \
-    main0101.hzip \
-    main0102.hzip \
-    main0103.hzip \
-    main0104.hzip \
-    main0105.hzip \
-    main0106.hzip \
-    main0200.hzip \
-    main0202.hzip \
-    main0210.hzip \
-    main0213.hzip \
-    main0503.hzip 
+   main0000.hzip \
+   main0100.hzip \
+   main0101.hzip \
+   main0102.hzip \
+   main0103.hzip \
+   main0104.hzip \
+   main0105.hzip \
+   main0106.hzip \
+   main0200.hzip \
+   main0202.hzip \
+   main0210.hzip \
+   main0213.hzip \
+   main0503.hzip 
     
 # --- Targets ------------------------------------------------------
 
