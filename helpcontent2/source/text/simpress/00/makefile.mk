668b4e0b3406201ae1c6eb2603abeb3a3ca88ed5
Rüdiger Timm <rt@openoffice.org>
2004-11-26 21:56:31 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:23 fpe 1.3.2.5: updated makefiles 2004/10/28 10:47:55 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:09 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:36 fpe 1.3.2.2: Regular Update 2004/09/16 11:48:16 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/simpress/00/makefile.mk
+++ helpcontent2/source/text/simpress/00/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:17 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 22:56:31 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/simpress/00
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_simpress_00
 # edit to match the current module
 MODULE  = simpress
@@ -32,15 +32,15 @@ MODULE  = simpress
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    00000004.hzip \
-    00000401.hzip \
-    00000402.hzip \
-    00000403.hzip \
-    00000404.hzip \
-    00000405.hzip \
-    00000406.hzip \
-    00000407.hzip \
-    00000413.hzip 
+   00000004.hzip \
+   00000401.hzip \
+   00000402.hzip \
+   00000403.hzip \
+   00000404.hzip \
+   00000405.hzip \
+   00000406.hzip \
+   00000407.hzip \
+   00000413.hzip 
     
 # --- Targets ------------------------------------------------------
 
