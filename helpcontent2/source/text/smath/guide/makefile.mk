79ac537e942de2a4fdf5902ce3249d29cc005c93
Rüdiger Timm <rt@openoffice.org>
2004-11-26 22:32:45 +0000

    INTEGRATION: CWS helpcontentbeta (1.3.2); FILE MERGED 2004/11/11 13:40:27 fpe 1.3.2.5: updated makefiles 2004/10/28 10:48:00 fpe 1.3.2.4: updated makefiles 2004/10/15 09:01:14 fpe 1.3.2.3: new makefiles 2004/09/16 12:26:41 fpe 1.3.2.2: Regular Update 2004/09/16 11:48:41 fpe 1.3.2.1: Regular Update

--- helpcontent2/source/text/smath/guide/makefile.mk
+++ helpcontent2/source/text/smath/guide/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: fpe $ $Date: 2004-08-19 19:54:20 $
+#*    last change       $Author: rt $ $Date: 2004-11-26 23:32:45 $
 #*
-#*    $Revision: 1.3 $
+#*    $Revision: 1.4 $
 #*
 #*    $Logfile:$
 #*
@@ -15,12 +15,12 @@
 
 # edit to match directory level 
 PRJ		= ..$/..$/..$/..
-# same for all makefiles in "help2"
-PRJNAME = help2
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
 # edit to match the current package
 PACKAGE = text/smath/guide
 # uniqe name (module wide);
-# using a modified forme of package should do here
+# using a modified form of package should do here
 TARGET  = text_smath_guide
 # edit to match the current module
 MODULE  = smath
@@ -32,14 +32,14 @@ MODULE  = smath
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
-    align.hzip \
-    attributes.hzip \
-    brackets.hzip \
-    comment.hzip \
-    keyboard.hzip \
-    main.hzip \
-    newline.hzip \
-    parentheses.hzip 
+   align.hzip \
+   attributes.hzip \
+   brackets.hzip \
+   comment.hzip \
+   keyboard.hzip \
+   main.hzip \
+   newline.hzip \
+   parentheses.hzip 
     
 # --- Targets ------------------------------------------------------
 
