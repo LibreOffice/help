a7724597d03dcdf7b21926f84511cc6078dd409e
Rüdiger Timm <rt@openoffice.org>
2004-12-16 11:49:29 +0000

    INTEGRATION: CWS helpaux (1.3.16); FILE MERGED 2004/12/08 17:42:35 fpe 1.3.16.4: updated makefiles 2004/12/08 12:08:59 fpe 1.3.16.3: conflict resolve 2004/12/07 11:44:10 fpe 1.3.16.2: RESYNC: (1.3-1.4); FILE MERGED 2004/11/30 18:30:37 fpe 1.3.16.1: *** empty log message ***

--- helpcontent2/source/text/scalc/02/makefile.mk
+++ helpcontent2/source/text/scalc/02/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-11-26 16:24:13 $
+#*    last change       $Author: rt $ $Date: 2004-12-16 12:49:29 $
 #*
-#*    $Revision: 1.4 $
+#*    $Revision: 1.5 $
 #*
 #*    $Logfile:$
 #*
@@ -27,8 +27,8 @@ MODULE  = scalc
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE : $(PRJ)$/settings.pmk
 .INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/settings.pmk
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
