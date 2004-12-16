554f4bbe9da347cad1153644f648569c8131e13b
Rüdiger Timm <rt@openoffice.org>
2004-12-16 11:59:42 +0000

    INTEGRATION: CWS helpaux (1.3.16); FILE MERGED 2004/12/08 17:42:55 fpe 1.3.16.4: updated makefiles 2004/12/08 12:09:21 fpe 1.3.16.3: conflict resolve 2004/12/07 13:13:17 fpe 1.3.16.2: RESYNC: (1.3-1.4); FILE MERGED 2004/11/30 18:30:58 fpe 1.3.16.1: *** empty log message ***

--- helpcontent2/source/text/swriter/04/makefile.mk
+++ helpcontent2/source/text/swriter/04/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-11-26 23:59:51 $
+#*    last change       $Author: rt $ $Date: 2004-12-16 12:59:42 $
 #*
-#*    $Revision: 1.4 $
+#*    $Revision: 1.5 $
 #*
 #*    $Logfile:$
 #*
@@ -27,8 +27,8 @@ MODULE  = swriter
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE : $(PRJ)$/settings.pmk
 .INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/settings.pmk
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
