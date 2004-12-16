14401c9f512df5409d78cf6dd1aa8c88cd7303e7
Rüdiger Timm <rt@openoffice.org>
2004-12-16 11:54:44 +0000

    INTEGRATION: CWS helpaux (1.3.16); FILE MERGED 2004/12/08 17:42:46 fpe 1.3.16.4: updated makefiles 2004/12/08 12:09:11 fpe 1.3.16.3: conflict resolve 2004/12/07 16:18:52 fpe 1.3.16.2: RESYNC: (1.3-1.4); FILE MERGED 2004/11/30 18:30:47 fpe 1.3.16.1: *** empty log message ***

--- helpcontent2/source/text/shared/autokorr/makefile.mk
+++ helpcontent2/source/text/shared/autokorr/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-11-26 21:33:01 $
+#*    last change       $Author: rt $ $Date: 2004-12-16 12:54:44 $
 #*
-#*    $Revision: 1.4 $
+#*    $Revision: 1.5 $
 #*
 #*    $Logfile:$
 #*
@@ -27,8 +27,8 @@ MODULE  = shared
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE : $(PRJ)$/settings.pmk
 .INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/settings.pmk
 
 # this list matches the *.xhp files to process
 HZIPFILES = \
