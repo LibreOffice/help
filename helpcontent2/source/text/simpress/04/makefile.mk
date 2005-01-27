51d0d95f497de709113d845b5ad2ce0eb25edec5
Rüdiger Timm <rt@openoffice.org>
2005-01-27 09:00:46 +0000

    INTEGRATION: CWS helpcl (1.5.2); FILE MERGED 2005/01/17 12:13:41 hjs 1.5.2.1: #i40787# changed makefiles for compile_while_link process

--- helpcontent2/source/text/simpress/04/makefile.mk
+++ helpcontent2/source/text/simpress/04/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-12-16 12:56:50 $
+#*    last change       $Author: rt $ $Date: 2005-01-27 10:00:46 $
 #*
-#*    $Revision: 1.5 $
+#*    $Revision: 1.6 $
 #*
 #*    $Logfile:$
 #*
@@ -31,8 +31,8 @@ MODULE  = simpress
 .INCLUDE : $(PRJ)$/settings.pmk
 
 # this list matches the *.xhp files to process
-HZIPFILES = \
-   01020000.hzip 
+XHPFILES = \
+   01020000.xhp 
     
 # --- Targets ------------------------------------------------------
 
