d47a3caaaa7f0e9f585eda919d616a4949fd548a
Rüdiger Timm <rt@openoffice.org>
2005-01-27 08:58:11 +0000

    INTEGRATION: CWS helpcl (1.5.2); FILE MERGED 2005/01/17 12:13:35 hjs 1.5.2.1: #i40787# changed makefiles for compile_while_link process

--- helpcontent2/source/text/shared/07/makefile.mk
+++ helpcontent2/source/text/shared/07/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-12-16 12:54:32 $
+#*    last change       $Author: rt $ $Date: 2005-01-27 09:58:11 $
 #*
-#*    $Revision: 1.5 $
+#*    $Revision: 1.6 $
 #*
 #*    $Logfile:$
 #*
@@ -31,8 +31,8 @@ MODULE  = shared
 .INCLUDE : $(PRJ)$/settings.pmk
 
 # this list matches the *.xhp files to process
-HZIPFILES = \
-   09000000.hzip 
+XHPFILES = \
+   09000000.xhp 
     
 # --- Targets ------------------------------------------------------
 
