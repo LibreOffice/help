a94ca7e5193b2883df9770b044263a5ffffc8bad
Jens-Heiner Rechtien <hr@openoffice.org>
2005-02-11 13:53:59 +0000

    INTEGRATION: CWS ause018 (1.2.16); FILE MERGED 2005/02/04 18:17:17 hjs 1.2.16.2: #i39062# create dirs before writing there 2005/02/04 16:40:33 hjs 1.2.16.1: #i39062# *.tree files generated now. en-US used as template

--- helpcontent2/source/auxiliary/makefile.mk
+++ helpcontent2/source/auxiliary/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: rt $ $Date: 2004-12-16 12:20:38 $
+#*    last change       $Author: hr $ $Date: 2005-02-11 14:53:59 $
 #*
-#*    $Revision: 1.2 $
+#*    $Revision: 1.3 $
 #*
 #*    $Logfile:$
 #*
@@ -16,19 +16,25 @@
 PRJ		= ..$/..
 PRJNAME = help2
 TARGET  = auxiliary
-# edit to match the current module
-MODULE  = sbasic
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE : settings.mk
 
-    
+TREEFILES  = \
+    sbasic.tree \
+    simpress.tree \
+    scalc.tree \
+    smath.tree \
+    schart.tree \
+    swriter.tree \
+    shared.tree
+
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
 
-ALLTAR : aux_dirs
+ALLTAR : aux_dirs $(MISC)$/treefiles.done
 
 AUX_DIR_LIST:=$(shell +$(FIND) . -type d ! -name "." | $(GREP) -v "CVS")
 
@@ -38,8 +44,19 @@ aux_langdirs:=$(shell +find ./ -type d ! -name CVS ! -name "." | sed "s/\.\/\///
 aux_langdirs:=$(subst,CVS, $(shell +-dir /ba:d ))
 .ENDIF          # "$(GUI)"=="UNX"
 
+aux_alllangiso:=$(foreach,i,$(alllangiso) $(foreach,j,$(aux_langdirs) $(eq,$i,$j  $i $(NULL))))
+
+LOCTREEFILES:=$(foreach,i,$(aux_alllangiso) $(foreach,j,$(TREEFILES) $(MISC)$/$i$/$j))
+
+$(MISC)$/treefiles.done : $(LOCTREEFILES)
+    @+$(PERL) $(PRJ)$/helpers$/update_tree.pl && $(TOUCH) $@
+
+%.created:
+    @+-$(MKDIRHIER) $(@:d) && $(TOUCH) $@
+
+$(LOCTREEFILES) : $(TREEFILES) $(PRJ)$/source$/text$/shared$/localize.sdf $$(@:d)$/dir.created
+    @+$(TOUCH) $@
 
 aux_dirs .PHONY :
     +echo aux_langdirs:=$(aux_langdirs) > $(INCCOM)$/aux_langs.mk
 
-#aux_alllangiso:=$(foreach,i,$(alllangiso) $(foreach,j,$(aux_langdirs) $(eq,$i,$j  $i $(NULL))))
