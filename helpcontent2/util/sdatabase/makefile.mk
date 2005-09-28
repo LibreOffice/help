ea69b148d4f9cd73f9aa19206f4c703f6f056e87
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-28 11:04:14 +0000

    INTEGRATION: CWS ause037 (1.6.50); FILE MERGED 2005/09/06 15:29:26 hjs 1.6.50.1: #125412# allow building to shared output tree

--- helpcontent2/util/sdatabase/makefile.mk
+++ helpcontent2/util/sdatabase/makefile.mk
@@ -3,15 +3,16 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2005-03-23 13:03:04 $
+#*    last change       $Author: hr $ $Date: 2005-09-28 12:04:14 $
 #*
-#*    $Revision: 1.6 $
+#*    $Revision: 1.7 $
 #*
 #*    $Logfile:$
 #*
 #*    Copyright 2000 Sun Microsystems, Inc. All Rights Reserved.
 #*
 #*************************************************************************
+
 # edit to match directory level 
 PRJ		= ..$/..
 # same for all makefiles in "helpcontent2"
@@ -31,7 +32,7 @@ zip1generatedlangs=TRUE
 zip1langdirs=$(aux_alllangiso)
 ZIP1TARGET=xhp_sdatabase
 ZIP1FLAGS= -u -r
-ZIP1DIR=$(MISC)$/$(LANGDIR)
+ZIP1DIR=$(COMMONMISC)$/$(LANGDIR)
 ZIP1LIST=$(LANGDIR)$/text$/shared$/explorer$/database$/main.xhp
 .ENDIF			# "$(SOLAR_JAVA)"!=""
 
