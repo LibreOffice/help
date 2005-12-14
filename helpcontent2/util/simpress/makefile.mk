793b27e45f3a67f44f3406ba6afde85c5d47fad6
Rüdiger Timm <rt@openoffice.org>
2005-12-14 14:35:37 +0000

    INTEGRATION: CWS ause044 (1.17.16); FILE MERGED 2005/11/28 17:25:45 hjs 1.17.16.1: #i58253# regenerated from helpers/linkmakefile.template

--- helpcontent2/util/simpress/makefile.mk
+++ helpcontent2/util/simpress/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: hr $ $Date: 2005-10-25 11:39:59 $
+#*    last change       $Author: rt $ $Date: 2005-12-14 15:35:37 $
 #*
-#*    $Revision: 1.17 $
+#*    $Revision: 1.18 $
 #*
 #*    $Logfile:$
 #*
@@ -33,7 +33,7 @@ zip1langdirs=$(aux_alllangiso)
 ZIP1TARGET=xhp_simpress
 ZIP1FLAGS= -u -r
 ZIP1DIR=$(COMMONMISC)$/$(LANGDIR)
-ZIP1LIST=$(LANGDIR)$/text$/simpress$/* -x "*.xhp????*"-x "*.dphh*" -x "*.hzip" -x "*.created"
+ZIP1LIST=$(LANGDIR)$/text$/simpress$/* -x "*.xhp????*" -x "*.dphh*" -x "*.hzip" -x "*.created"
 .ENDIF			# "$(SOLAR_JAVA)"!=""
 
 
