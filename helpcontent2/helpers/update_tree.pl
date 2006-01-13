44697a64c6fe1fd5f4ee4873832731ba8b8e9161
Rüdiger Timm <rt@openoffice.org>
2006-01-13 15:16:20 +0000

    INTEGRATION: CWS ause046 (1.8.46); FILE MERGED 2006/01/05 16:12:55 hjs 1.8.46.1: #i60083# add license header

--- helpcontent2/helpers/update_tree.pl
+++ helpcontent2/helpers/update_tree.pl
@@ -1,4 +1,40 @@
-#!/usr/bin/perl
+:
+eval 'exec perl -wS $0 ${1+"$@"}'
+    if 0;
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: update_tree.pl,v $
+#
+#   $Revision: 1.10 $
+#
+#   last change: $Author: rt $ $Date: 2006-01-13 16:16:20 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
 
 use Cwd 'abs_path';
 use File::Find;
