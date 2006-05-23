9463b639b4446a87a1771e990bfb4cbde92dd772
Vladimir Glazounov <vg@openoffice.org>
2006-05-23 15:00:46 +0000

    INTEGRATION: CWS hc680dec (1.18.18); FILE MERGED 2006/05/22 13:11:21 ufi 1.18.18.5: RESYNC: (1.19-1.22); FILE MERGED 2006/03/20 13:13:23 ufi 1.18.18.4: RESYNC: (1.18-1.19); FILE MERGED 2006/01/26 12:35:54 ufi 1.18.18.3: final 2006/01/20 14:12:19 ufi 1.18.18.2: friday again 2005/12/09 13:45:14 ufi 1.18.18.1: makefiles

--- helpcontent2/util/scalc/makefile.mk
+++ helpcontent2/util/scalc/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2006-05-17 15:28:00 $
+#*    last change       $Author: vg $ $Date: 2006-05-23 16:00:46 $
 #*
-#*    $Revision: 1.22 $
+#*    $Revision: 1.23 $
 #*
 #*    $Logfile:$
 #*
@@ -645,7 +645,6 @@ LINKLINKFILES= \
    text$/shared$/01$/06140102.xhp \
    text$/shared$/01$/06140200.xhp \
    text$/shared$/01$/06140400.xhp \
-   text$/shared$/01$/06140401.xhp \
    text$/shared$/01$/06140402.xhp \
    text$/shared$/01$/06140500.xhp \
    text$/shared$/01$/06150000.xhp \
@@ -663,6 +662,7 @@ LINKLINKFILES= \
    text$/shared$/01$/08060100.xhp \
    text$/shared$/01$/about_meta_tags.xhp \
    text$/shared$/01$/digitalsignatures.xhp \
+   text$/shared$/01$/formatting_mark.xhp \
    text$/shared$/01$/gallery.xhp \
    text$/shared$/01$/gallery_files.xhp \
    text$/shared$/01$/grid.xhp \
@@ -962,7 +962,6 @@ LINKLINKFILES= \
    text$/shared$/explorer$/database$/11030000.xhp \
    text$/shared$/explorer$/database$/11030100.xhp \
    text$/shared$/explorer$/database$/11040000.xhp \
-   text$/shared$/explorer$/database$/11060000.xhp \
    text$/shared$/explorer$/database$/11080000.xhp \
    text$/shared$/explorer$/database$/11090000.xhp \
    text$/shared$/explorer$/database$/11110000.xhp \
@@ -1177,11 +1176,9 @@ LINKLINKFILES= \
    text$/shared$/optionen$/01010700.xhp \
    text$/shared$/optionen$/01010800.xhp \
    text$/shared$/optionen$/01010900.xhp \
-   text$/shared$/optionen$/01010950.xhp \
    text$/shared$/optionen$/01011000.xhp \
    text$/shared$/optionen$/01012000.xhp \
    text$/shared$/optionen$/01013000.xhp \
-   text$/shared$/optionen$/01014000.xhp \
    text$/shared$/optionen$/01020000.xhp \
    text$/shared$/optionen$/01020100.xhp \
    text$/shared$/optionen$/01020200.xhp \
