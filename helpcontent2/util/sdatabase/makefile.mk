2d217e7513c69b9aa4ef3a797fffb6e3f71b1067
Kurt Zenker <kz@openoffice.org>
2006-10-05 12:49:16 +0000

    INTEGRATION: CWS hcshared04 (1.10.2); FILE MERGED 2006/08/11 13:44:32 fpe 1.10.2.2: *** empty log message *** 2006/08/01 12:31:37 ufi 1.10.2.1: new files

--- helpcontent2/util/sdatabase/makefile.mk
+++ helpcontent2/util/sdatabase/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2006-08-17 14:06:35 $
+#*    last change       $Author: kz $ $Date: 2006-10-05 13:49:16 $
 #*
-#*    $Revision: 1.12 $
+#*    $Revision: 1.13 $
 #*
 #*    $Logfile:$
 #*
@@ -918,6 +918,7 @@ LINKLINKFILES= \
    text$/shared$/optionen$/macrosecurity_sl.xhp \
    text$/shared$/optionen$/macrosecurity_ts.xhp \
    text$/shared$/optionen$/mailmerge.xhp \
+   text$/shared$/optionen$/online_update.xhp \
    text$/shared$/optionen$/serverauthentication.xhp \
    text$/shared$/optionen$/testaccount.xhp \
    text$/shared$/optionen$/viewcertificate.xhp \
