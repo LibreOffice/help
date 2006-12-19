516070b867f84f1c5d14067b6f1ee02877f9c37e
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 15:29:50 +0000

    INTEGRATION: CWS aw038 (1.24.28); FILE MERGED 2006/12/08 10:21:16 ufi 1.24.28.1: new Name and Description commands

--- helpcontent2/util/sbasic/makefile.mk
+++ helpcontent2/util/sbasic/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: kz $ $Date: 2006-10-05 13:48:36 $
+#*    last change       $Author: ihi $ $Date: 2006-12-19 16:29:50 $
 #*
-#*    $Revision: 1.24 $
+#*    $Revision: 1.25 $
 #*
 #*    $Logfile:$
 #*
@@ -549,6 +549,7 @@ LINKLINKFILES= \
    text$/shared$/01$/05140100.xhp \
    text$/shared$/01$/05150101.xhp \
    text$/shared$/01$/05190000.xhp \
+   text$/shared$/01$/05190100.xhp \
    text$/shared$/01$/05200000.xhp \
    text$/shared$/01$/05200100.xhp \
    text$/shared$/01$/05200200.xhp \
