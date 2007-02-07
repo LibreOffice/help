7c28078cb6620ad29d7ac63d84dcbd98c88d1d79
Vladimir Glazounov <vg@openoffice.org>
2007-02-07 11:04:11 +0000

    INTEGRATION: CWS hcshared07 (1.27.18); FILE MERGED 2007/01/02 15:51:21 ufi 1.27.18.1: zwischenstand

--- helpcontent2/util/swriter/makefile.mk
+++ helpcontent2/util/swriter/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: ihi $ $Date: 2006-12-19 16:31:13 $
+#*    last change       $Author: vg $ $Date: 2007-02-07 12:04:11 $
 #*
-#*    $Revision: 1.28 $
+#*    $Revision: 1.29 $
 #*
 #*    $Logfile:$
 #*
@@ -762,6 +762,7 @@ LINKLINKFILES= \
    text$/shared$/01$/guides.xhp \
    text$/shared$/01$/mediaplayer.xhp \
    text$/shared$/01$/moviesound.xhp \
+   text$/shared$/01$/online_update.xhp \
    text$/shared$/01$/packagemanager.xhp \
    text$/shared$/01$/password_dlg.xhp \
    text$/shared$/01$/password_main.xhp \
