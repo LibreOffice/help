6ad29cd5950a271212fdd1a4b7b64b2956ab9157
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 09:21:07 +0000

    INTEGRATION: CWS helplinker01 (1.16.156); FILE MERGED 2007/05/20 14:01:31 cmc 1.16.156.1: #i70155# use native HelpLinker

--- helpcontent2/util/schart/makefile.mk
+++ helpcontent2/util/schart/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2007-05-22 16:01:20 $
+#*    last change       $Author: ihi $ $Date: 2007-06-05 10:21:07 $
 #*
-#*    $Revision: 1.17 $
+#*    $Revision: 1.18 $
 #*
 #*    $Logfile:$
 #*
@@ -26,7 +26,6 @@ TARGET  = util_schart
 .INCLUDE : settings.mk
 .INCLUDE : $(PRJ)$/settings.pmk
 
-.IF "$(SOLAR_JAVA)"!=""
 common_build_zip:=
 zip1generatedlangs=TRUE
 zip1langdirs=$(aux_alllangiso)
@@ -34,8 +33,6 @@ ZIP1TARGET=xhp_schart
 ZIP1FLAGS= -u -r
 ZIP1DIR=$(COMMONMISC)$/$(LANGDIR)
 ZIP1LIST=$(LANGDIR)$/text$/schart$/* -x "*.xhp????*" -x "*.dphh*" -x "*.hzip" -x "*.created"
-.ENDIF			# "$(SOLAR_JAVA)"!=""
-
 
 LINKNAME=schart
 LINKADDEDFILES= \
