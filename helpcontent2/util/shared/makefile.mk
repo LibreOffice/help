8008337ca00d44a600532cd0a635bc9c5920add4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-26 11:35:57 +0000

    INTEGRATION: CWS helplinker01redux (1.17.2); FILE MERGED 2007/06/13 13:07:17 cmc 1.17.2.1: #i70155 native HelpLinker

--- helpcontent2/util/shared/makefile.mk
+++ helpcontent2/util/shared/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2007-06-08 16:29:59 $
+#*    last change       $Author: hr $ $Date: 2007-06-26 12:35:57 $
 #*
-#*    $Revision: 1.17 $
+#*    $Revision: 1.18 $
 #*
 #*    $Logfile:$
 #*
@@ -26,7 +26,6 @@ TARGET  = util_shared
 .INCLUDE : settings.mk
 .INCLUDE : $(PRJ)$/settings.pmk
 
-.IF "$(SOLAR_JAVA)"!=""
 common_build_zip:=
 zip1generatedlangs=TRUE
 zip1langdirs=$(aux_alllangiso)
@@ -34,8 +33,6 @@ ZIP1TARGET=xhp_shared
 ZIP1FLAGS= -u -r
 ZIP1DIR=$(COMMONMISC)$/$(LANGDIR)
 ZIP1LIST=$(LANGDIR)$/text$/shared$/* -x "*.xhp????*" -x "*.dphh*" -x "*.hzip" -x "*.created"
-.ENDIF			# "$(SOLAR_JAVA)"!=""
-
 
 LINKNAME=shared
 LINKADDEDFILES= \
