fac525d36c7cf47fe55ddc9ffab3b62a4b0d1bcc
Vladimir Glazounov <vg@openoffice.org>
2007-08-31 15:37:18 +0000

    #100000# 01120000.xhp is deleted by hcshared09 CWS

--- helpcontent2/util/schart/makefile.mk
+++ helpcontent2/util/schart/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: hr $ $Date: 2007-07-31 15:42:54 $
+#*    last change       $Author: vg $ $Date: 2007-08-31 16:37:18 $
 #*
-#*    $Revision: 1.21 $
+#*    $Revision: 1.22 $
 #*
 #*    $Logfile:$
 #*
@@ -120,7 +120,6 @@ LINKLINKFILES= \
    text$/shared$/01$/01110101.xhp \
    text$/shared$/01$/01110300.xhp \
    text$/shared$/01$/01110400.xhp \
-   text$/shared$/01$/01120000.xhp \
    text$/shared$/01$/01130000.xhp \
    text$/shared$/01$/01140000.xhp \
    text$/shared$/01$/01160000.xhp \
