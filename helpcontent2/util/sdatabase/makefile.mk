8d4fba48d4fdd193d66641f4cb6f154bdf9652d2
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 17:24:38 +0000

    INTEGRATION: CWS hcshared12 (1.23.2); FILE MERGED 2007/10/30 13:56:38 ufi 1.23.2.4: Zwischenstand 2007/10/16 11:05:00 ufi 1.23.2.3: makefiles 2007/10/09 14:18:07 ufi 1.23.2.2: new makefiles 2007/10/02 11:18:51 ufi 1.23.2.1: makefiles

--- helpcontent2/util/sdatabase/makefile.mk
+++ helpcontent2/util/sdatabase/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: vg $ $Date: 2007-09-21 10:20:21 $
+#*    last change       $Author: ihi $ $Date: 2007-11-23 18:24:38 $
 #*
-#*    $Revision: 1.23 $
+#*    $Revision: 1.24 $
 #*
 #*    $Logfile:$
 #*
@@ -334,7 +334,6 @@ LINKLINKFILES= \
    text$/shared$/01$/06140102.xhp \
    text$/shared$/01$/06140200.xhp \
    text$/shared$/01$/06140400.xhp \
-   text$/shared$/01$/06140401.xhp \
    text$/shared$/01$/06140402.xhp \
    text$/shared$/01$/06140500.xhp \
    text$/shared$/01$/06150000.xhp \
@@ -773,6 +772,7 @@ LINKLINKFILES= \
    text$/shared$/guide$/fontwork.xhp \
    text$/shared$/guide$/formfields.xhp \
    text$/shared$/guide$/gallery_insert.xhp \
+   text$/shared$/guide$/groups.xhp \
    text$/shared$/guide$/hyperlink_edit.xhp \
    text$/shared$/guide$/hyperlink_insert.xhp \
    text$/shared$/guide$/hyperlink_rel_abs.xhp \
