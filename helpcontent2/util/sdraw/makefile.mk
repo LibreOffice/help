0a71e722ab2560e1e01e34d0a31d906d43fa0a21
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-26 13:30:37 +0000

    INTEGRATION: CWS aw053 (1.33.4); FILE MERGED 2007/11/01 18:12:43 aw 1.33.4.2: RESYNC: (1.33-1.35); FILE MERGED 2007/09/17 13:27:11 ufi 1.33.4.1: i81634 makefiles

--- helpcontent2/util/sdraw/makefile.mk
+++ helpcontent2/util/sdraw/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: ihi $ $Date: 2007-11-23 18:24:56 $
+#*    last change       $Author: ihi $ $Date: 2007-11-26 14:30:37 $
 #*
-#*    $Revision: 1.36 $
+#*    $Revision: 1.37 $
 #*
 #*    $Logfile:$
 #*
@@ -217,10 +217,6 @@ LINKLINKFILES= \
    text$/simpress$/02$/13170000.xhp \
    text$/simpress$/02$/13180000.xhp \
    text$/simpress$/02$/13190000.xhp \
-   text$/simpress$/02$/13200000.xhp \
-   text$/simpress$/02$/13210000.xhp \
-   text$/simpress$/02$/13220000.xhp \
-   text$/simpress$/02$/13230000.xhp \
    text$/simpress$/04$/01020000.xhp \
    text$/simpress$/guide$/3d_create.xhp \
    text$/simpress$/guide$/animated_gif_create.xhp \
