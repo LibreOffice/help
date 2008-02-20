e464e972449e3e6c422b8dad03a15ccce0b0303a
Rüdiger Timm <rt@openoffice.org>
2008-02-20 00:58:07 +0000

    INTEGRATION: CWS hcshared16 (1.24.14); FILE MERGED 2008/02/13 11:16:03 ufi 1.24.14.5: makefiles 2008/02/11 13:19:11 ufi 1.24.14.4: changed how-to-tables 2008/02/04 11:28:37 mwaller 1.24.14.3: new file 2008/01/30 08:09:51 ufi 1.24.14.2: new makefiles 2008/01/22 13:12:50 ufi 1.24.14.1: new files

--- helpcontent2/util/sdatabase/makefile.mk
+++ helpcontent2/util/sdatabase/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: ihi $ $Date: 2007-11-23 18:24:38 $
+#*    last change       $Author: rt $ $Date: 2008-02-20 01:58:07 $
 #*
-#*    $Revision: 1.24 $
+#*    $Revision: 1.25 $
 #*
 #*    $Logfile:$
 #*
@@ -56,7 +56,6 @@ LINKLINKFILES= \
    text$/shared$/00$/00000011.xhp \
    text$/shared$/00$/00000020.xhp \
    text$/shared$/00$/00000021.xhp \
-   text$/shared$/00$/00000040.xhp \
    text$/shared$/00$/00000099.xhp \
    text$/shared$/00$/00000202.xhp \
    text$/shared$/00$/00000203.xhp \
