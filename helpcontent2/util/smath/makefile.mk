6f879865740f054cb98a4775f5653aecde53de8a
Kurt Zenker <kz@openoffice.org>
2008-03-07 11:06:07 +0000

    INTEGRATION: CWS calcshare_DEV300 (1.35.4); FILE MERGED 2008/03/04 12:10:24 ufi 1.35.4.1: collab_changes

--- helpcontent2/util/smath/makefile.mk
+++ helpcontent2/util/smath/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: kz $ $Date: 2008-03-07 11:58:24 $
+#*    last change       $Author: kz $ $Date: 2008-03-07 12:06:07 $
 #*
-#*    $Revision: 1.37 $
+#*    $Revision: 1.38 $
 #*
 #*    $Logfile:$
 #*
@@ -806,6 +806,7 @@ LINKLINKFILES= \
    text$/shared$/guide$/chart_insert.xhp \
    text$/shared$/guide$/chart_legend.xhp \
    text$/shared$/guide$/chart_title.xhp \
+   text$/shared$/guide$/collab.xhp \
    text$/shared$/guide$/configure_overview.xhp \
    text$/shared$/guide$/contextmenu.xhp \
    text$/shared$/guide$/copy_drawfunctions.xhp \
