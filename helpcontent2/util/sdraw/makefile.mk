9439070221cd249fcd0e9dc38f57d7a23341f1cf
Kurt Zenker <kz@openoffice.org>
2008-03-07 11:41:35 +0000

    INTEGRATION: CWS hcshared17_DEV300 (1.38.2); FILE MERGED 2008/03/04 15:24:12 ufi 1.38.2.1: vor build

--- helpcontent2/util/sdraw/makefile.mk
+++ helpcontent2/util/sdraw/makefile.mk
@@ -3,9 +3,9 @@
 #*    $Workfile:$
 #*
 #*    Creation date     KR 28.06.99
-#*    last change       $Author: kz $ $Date: 2008-03-07 12:05:29 $
+#*    last change       $Author: kz $ $Date: 2008-03-07 12:41:35 $
 #*
-#*    $Revision: 1.40 $
+#*    $Revision: 1.41 $
 #*
 #*    $Logfile:$
 #*
@@ -979,6 +979,8 @@ LINKLINKFILES= \
    text$/shared$/guide$/data_view.xhp \
    text$/shared$/guide$/database_main.xhp \
    text$/shared$/guide$/digital_signatures.xhp \
+   text$/shared$/guide$/digitalsign_receive.xhp \
+   text$/shared$/guide$/digitalsign_send.xhp \
    text$/shared$/guide$/doc_autosave.xhp \
    text$/shared$/guide$/doc_open.xhp \
    text$/shared$/guide$/doc_save.xhp \
@@ -1059,6 +1061,8 @@ LINKLINKFILES= \
    text$/shared$/guide$/workfolder.xhp \
    text$/shared$/guide$/xforms.xhp \
    text$/shared$/guide$/xsltfilter.xhp \
+   text$/shared$/guide$/xsltfilter_create.xhp \
+   text$/shared$/guide$/xsltfilter_distribute.xhp \
    text$/shared$/main0108.xhp \
    text$/shared$/main0201.xhp \
    text$/shared$/main0208.xhp \
