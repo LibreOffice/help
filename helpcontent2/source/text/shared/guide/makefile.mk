12df3adcb43465beb04bea458e14a5c3e903c4e7
Vladimir Glazounov <vg@openoffice.org>
2008-06-03 07:59:25 +0000

    INTEGRATION: CWS hcshared18 (1.13.2); FILE MERGED 2008/05/28 18:15:10 fpe 1.13.2.5: remove DOCTYPE headers from source files to avoid performance problems in installed help 2008/05/27 13:55:26 ufi 1.13.2.4: makefiles 2008/05/21 09:12:23 ufi 1.13.2.3: vor testbuild 2008/04/23 09:53:55 fpe 1.13.2.2: committing changes to makefile and xhps 2008/03/12 12:13:42 ufi 1.13.2.1: makefiles

--- helpcontent2/source/text/shared/guide/makefile.mk
+++ helpcontent2/source/text/shared/guide/makefile.mk
@@ -1,16 +1,32 @@
 #*************************************************************************
-#*
-#*    $Workfile:$
-#*
-#*    Creation date     KR 28.06.99
-#*    last change       $Author: kz $ $Date: 2008-03-07 12:24:46 $
-#*
-#*    $Revision: 1.13 $
-#*
-#*    $Logfile:$
-#*
-#*    Copyright 2000 Sun Microsystems, Inc. All Rights Reserved.
-#*
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.14 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
 #*************************************************************************
 
 # edit to match directory level 
