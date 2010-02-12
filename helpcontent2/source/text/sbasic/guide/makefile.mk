4cb21c3a3e38acc5a1dad5835613c1b2e539abf7
Jens-Heiner Rechtien <hr@openoffice.org>
2010-02-12 15:01:35 +0100

    changefileheader2: #i109125#: change source file copyright notice from Sun Microsystems to Oracle; remove CVS style keywords (RCSfile, Revision)

--- helpcontent2/source/text/sbasic/guide/makefile.mk
+++ helpcontent2/source/text/sbasic/guide/makefile.mk
@@ -2,13 +2,9 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 # 
-# Copyright 2008 by Sun Microsystems, Inc.
+# Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
-#
-# $RCSfile: makefile.template,v $
-#
-# $Revision: 1.5 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -54,7 +50,7 @@ XHPFILES = \
    sample_code.xhp \
    show_dialog.xhp \
    translation.xhp 
-    
+	
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
--- helpcontent2/source/text/swriter/04/makefile.mk
+++ helpcontent2/source/text/swriter/04/makefile.mk
@@ -2,13 +2,9 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 # 
-# Copyright 2008 by Sun Microsystems, Inc.
+# Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
-#
-# $RCSfile: makefile.template,v $
-#
-# $Revision: 1.5 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -49,7 +45,7 @@ MODULE  = swriter
 # this list matches the *.xhp files to process
 XHPFILES = \
    01020000.xhp 
-    
+	
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
