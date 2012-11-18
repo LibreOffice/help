3499c33c54d33e9654c77741eb4276cdd57ed981
Ztamas <zolnaitamas2000g@gmail.com>
2012-11-18 16:21:51 +0100

    Localize tree files in help
    -Use the new treex instead of update_tree.pl
    -Change dependancies to localize xhp files
     before tree files because treex use localized
     xhp files.
    
    Change-Id: Ie061fc497542f53f81af9b6201bab1ef04d2020b

--- source/auxiliary/makefile.mk
+++ source/auxiliary/makefile.mk
@@ -34,15 +34,6 @@ TARGET  = auxiliary
 
 .INCLUDE : settings.mk
 
-TREEFILES  = \
-    sbasic.tree \
-    simpress.tree \
-    scalc.tree \
-    smath.tree \
-    schart.tree \
-    swriter.tree \
-    shared.tree
-
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
@@ -50,7 +41,7 @@ TREEFILES  = \
 XMLLINT !:=$(XMLLINT)
 .EXPORT : XMLLINT
 
-ALLTAR : aux_dirs $(COMMONMISC)$/treefiles.done $(COMMONBIN)$/helpimg.ilst
+ALLTAR : aux_dirs $(COMMONBIN)$/helpimg.ilst
 
 .IF "$(WITH_LANG)"!=""
 t_aux_langdirs:=$(shell @find ./ -name "*.cfg" | sed "s/\.\/\///" | sed "s/\.\///" )
@@ -58,41 +49,11 @@ t_aux_langdirs:=$(shell @find ./ -name "*.cfg" | sed "s/\.\/\///" | sed "s/\.\//
 aux_langdirs:=$(uniq $(t_aux_langdirs:d:d))
 help_exist:=$(shell @find $(L10N_MODULE)/source/ -type d -name "helpcontent2" | sed -e "s|/helpcontent2||" -e "s|^.*/||" ) en-US qtz
 
-aux_alllangiso_all:=$(foreach,i,$(alllangiso) $(foreach,j,$(aux_langdirs) $(eq,$i,$j  $i $(NULL))))
-aux_alllangiso:=$(foreach,i,$(aux_alllangiso_all) $(foreach,j,$(help_exist) $(eq,$i,$j  $i $(NULL))))
-
-WITH_LANG!:=$(aux_alllangiso)
-.EXPORT : WITH_LANG
-
-LOCTREEFILES:=$(foreach,i,$(aux_alllangiso) $(foreach,j,$(TREEFILES) $(COMMONMISC)$/$i$/$j))
 .ELSE			#IF "$(WITH_LANG)"!=""
 aux_langdirs:=en-US
 help_exist:=en-US
-LOCTREEFILES:=$(foreach,j,$(TREEFILES) $(COMMONMISC)$/en-US$/$j)
+
 .ENDIF			#IF "$(WITH_LANG)"!=""
-
-$(COMMONMISC)$/treefiles.done : $(LOCTREEFILES)
-    $(COMMAND_ECHO)+$(PERL) $(PRJ)$/helpers$/update_tree.pl && $(TOUCH) $@
-
-%.created:
-    @-$(MKDIRHIER) $(@:d) && $(TOUCH) $@
-
-$(LOCTREEFILES) : $(TREEFILES) $$(@:d)$/dir.created
-    @$(TOUCH) $@
-
-.IF "$(WITH_LANG)"!=""
-.IF "$(LOCALIZATION_FOUND)"=="YES"
-$(LOCTREEFILES) : $(TRYSDF:d:d:d:d)$/text$/shared/localize.sdf
-.ELSE			# "$(LOCALIZATION_FOUND)"=="YES"
-$(COMMONMISC)/unpack.done : $(SOLARCOMMONSDFDIR)$/$(PRJNAME).zip
-    @@-$(MKDIRHIER) $(COMMONMISC)$/$(PRJNAME)_$(TARGET)
-    unzip -o -d $(COMMONMISC)$/$(PRJNAME) $(SOLARCOMMONSDFDIR)$/$(PRJNAME).zip 
-    $(TOUCH) $@
-
-$(LOCTREEFILES) : $(COMMONMISC)/unpack.done
-
-.ENDIF			# "$(LOCALIZATION_FOUND)"=="YES"
-.ENDIF			# "$(WITH_LANG)"!=""
 
 aux_dirs .PHONY :
     @echo aux_langdirs:=$(aux_langdirs) > $(LOCAL_COMMON_OUT)/inc$/aux_langs.mk
---
+++ source/text/makefile.mk
@@ -0,0 +1,88 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2000, 2010 Oracle and/or its affiliates.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+
+PRJ		= ..$/..
+PRJNAME = helpcontent2
+TARGET  = text
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/settings.pmk
+
+TREEFILES  = \
+    sbasic.tree \
+    simpress.tree \
+    scalc.tree \
+    smath.tree \
+    schart.tree \
+    swriter.tree \
+    shared.tree
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+
+ALLTAR : $(COMMONMISC)$/treefiles.done
+
+.IF "$(WITH_LANG)"!=""
+
+WITH_LANG!:=$(aux_alllangiso)
+.EXPORT : WITH_LANG
+
+LOCTREEFILES:=$(foreach,i,$(aux_alllangiso) $(foreach,j,$(TREEFILES) $(COMMONMISC)$/$i$/$j))
+.ELSE			#IF "$(WITH_LANG)"!=""
+
+LOCTREEFILES:=$(foreach,j,$(TREEFILES) $(COMMONMISC)$/en-US$/$j)
+.ENDIF			#IF "$(WITH_LANG)"!=""
+
+$(COMMONMISC)$/treefiles.done : $(LOCTREEFILES)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(foreach,tree,$(TREEFILES) \
+            $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/$(tree) -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/$(tree) -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && )) \
+    $(TOUCH) $@
+
+%.created:
+    @-$(MKDIRHIER) $(@:d) && $(TOUCH) $@
+
+$(LOCTREEFILES) : $(foreach,tree,$(TREEFILES) ../auxiliary/$(tree)) $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+.IF "$(WITH_LANG)"!=""
+.IF "$(LOCALIZATION_FOUND)"=="YES"
+$(LOCTREEFILES) : $(TRYSDF:d:d:d:d)$/text$/shared/localize.sdf
+.ELSE			# "$(LOCALIZATION_FOUND)"=="YES"
+$(COMMONMISC)/unpack.done : $(SOLARCOMMONSDFDIR)$/$(PRJNAME).zip
+    @@-$(MKDIRHIER) $(COMMONMISC)$/$(PRJNAME)_$(TARGET)
+    unzip -o -d $(COMMONMISC)$/$(PRJNAME) $(SOLARCOMMONSDFDIR)$/$(PRJNAME).zip 
+    $(TOUCH) $@
+
+$(LOCTREEFILES) : $(COMMONMISC)/unpack.done
+
+.ENDIF			# "$(LOCALIZATION_FOUND)"=="YES"
+.ENDIF			# "$(WITH_LANG)"!=""
