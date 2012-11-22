4791cbfa5e27f68938d0219ecb1b200edf6443f4
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-11-22 13:23:37 +0100

    Too long commandline fix
    Change-Id: Iba6165afe9899a3fd864e862706075313d724132

--- source/text/makefile.mk
+++ source/text/makefile.mk
@@ -36,41 +36,91 @@ TARGET  = text
 .INCLUDE : settings.mk
 .INCLUDE : $(PRJ)$/settings.pmk
 
-TREEFILES  = \
-    sbasic.tree \
-    simpress.tree \
-    scalc.tree \
-    smath.tree \
-    schart.tree \
-    swriter.tree \
-    shared.tree
-
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
 
-ALLTAR : $(COMMONMISC)$/treefiles.done
+ALLTAR : $(COMMONMISC)$/sbasic.tree.done $(COMMONMISC)$/simpress.tree.done $(COMMONMISC)$/scalc.tree.done $(COMMONMISC)$/smath.tree.done $(COMMONMISC)$/schart.tree.done $(COMMONMISC)$/swriter.tree.done $(COMMONMISC)$/shared.tree.done
 
 .IF "$(WITH_LANG)"!=""
 
 WITH_LANG!:=$(aux_alllangiso)
 .EXPORT : WITH_LANG
 
-LOCTREEFILES:=$(foreach,i,$(aux_alllangiso) $(foreach,j,$(TREEFILES) $(COMMONMISC)$/$i$/$j))
+LOCSBASIC:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/sbasic.tree)
+LOCSIMPRESS:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/simpress.tree)
+LOCSCALC:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/scalc.tree)
+LOCSMATH:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/smath.tree)
+LOCSCHART:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/schart.tree)
+LOCSWRITER:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/swriter.tree)
+LOCSHARED:=$(foreach,i,$(aux_alllangiso) $(COMMONMISC)$/$i$/shared.tree)
 .ELSE			#IF "$(WITH_LANG)"!=""
 
-LOCTREEFILES:=$(foreach,j,$(TREEFILES) $(COMMONMISC)$/en-US$/$j)
+LOCSBASIC:=$(COMMONMISC)$/en_US$/sbasic.tree
+LOCSIMPRESS:=$(COMMONMISC)$/en_US$/simpress.tree
+LOCSCALC:=$(COMMONMISC)$/en_US$/scalc.tree
+LOCSMATH:=$(COMMONMISC)$/en_US$/smath.tree
+LOCSCHART:=$(COMMONMISC)$/en_US$/schart.tree
+LOCSWRITER:=$(COMMONMISC)$/en_US$/swriter.tree
+LOCSHARED:=$(COMMONMISC)$/en_US$/shared.tree
 .ENDIF			#IF "$(WITH_LANG)"!=""
 
-$(COMMONMISC)$/treefiles.done : $(LOCTREEFILES)
+$(COMMONMISC)$/sbasic.tree.done : $(LOCSBASIC)
     @$(foreach,lang,$(aux_alllangiso) \
-        $(foreach,tree,$(TREEFILES) \
-            $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/$(tree) -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/$(tree) -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && )) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/sbasic.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/sbasic.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/simpress.tree.done : $(LOCSIMPRESS)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/simpress.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/simpress.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/scalc.tree.done : $(LOCSCALC)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/scalc.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/scalc.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/smath.tree.done : $(LOCSMATH)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/smath.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/smath.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/schart.tree.done : $(LOCSCHART)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/schart.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/schart.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/swriter.tree.done : $(LOCSWRITER)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/swriter.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/swriter.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
+    $(TOUCH) $@
+
+$(COMMONMISC)$/shared.tree.done : $(LOCSHARED)
+    @$(foreach,lang,$(aux_alllangiso) \
+        $(AUGMENT_LIBRARY_PATH) $(TREEX) -i $(SRC_ROOT)/$(PRJNAME)/source/auxiliary/shared.tree -o $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/shared.tree -l $(lang) $(eq,lang,en-US $(NULL) -m $(mktmp $(SRC_ROOT)/translations/source/$(lang)/$(PRJNAME)/source/auxiliary.po)) -r $(subst,$(PRJ),$(SRC_ROOT)/$(PRJNAME) $(COMMONMISC))/$(lang)/text && ) \
     $(TOUCH) $@
 
 %.created:
     @-$(MKDIRHIER) $(@:d) && $(TOUCH) $@
 
-$(LOCTREEFILES) : $(foreach,tree,$(TREEFILES) ../auxiliary/$(tree)) $$(@:d)$/dir.created
+$(LOCSBASIC) : ../auxiliary/sbasic.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSIMPRESS) : ../auxiliary/simpress.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSCALC) : ../auxiliary/scalc.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSMATH) : ../auxiliary/smath.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSCHART) : ../auxiliary/schart.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSWRITER) : ../auxiliary/swriter.tree $$(@:d)$/dir.created
+    @$(TOUCH) $@
+
+$(LOCSHARED) : ../auxiliary/shared.tree $$(@:d)$/dir.created
     @$(TOUCH) $@
 
