72a7cc3cb27e697123288339ed66fb0d74534f40
tagezi <lera.goncharuk@gmail.com>
2015-10-06 23:41:14 +0300

    Added discription AVERAGEIFS
    func_averageif.xhp - corrected typos
    	added ODF 1.2
    	added the link to AVERAGEIFS
    
    Change-Id: I659991e24418f5328780ca67eef5d3e939e22e7d

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -183,6 +183,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/format_graphic \
     helpcontent2/source/text/scalc/01/func_aggregate \
     helpcontent2/source/text/scalc/01/func_averageif \
+    helpcontent2/source/text/scalc/01/func_averageifs \
     helpcontent2/source/text/scalc/01/func_date \
     helpcontent2/source/text/scalc/01/func_datedif \
     helpcontent2/source/text/scalc/01/func_datevalue \
