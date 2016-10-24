b354332fb6f6c65b9f5eec31291324df1ce3691f
Fabio Biocchetti <fbbcchtt9@gmail.com>
2016-10-24 19:16:27 +0200

    Fix comments in script and JavaScript refactor.
    Comments correction in shell script
    Introduced js debouncer for search bar
    xml-stylesheet tag removed
    Introduced parameter passing through pages
    Links in XHP pages now work with js
    Minor adjustments
    
    Change-Id: Ibf5f7a74c3249fa6f1ff4d3037fe821797e88d1d

--- help3xsl/get_bookmark.sh
+++ help3xsl/get_bookmark.sh
@@ -1,11 +1,10 @@
 #/bin/bash
-#*--
-#* This file is part of the LibreOffice project.
-#*
-#* This Source Code Form is subject to the terms of the Mozilla Public
-#* License, v. 2.0. If a copy of the MPL was not distributed with this
-#* file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#*
+
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 stub1='<html><head><link rel="stylesheet" type="text/css" href="/default.css"/></head><body><ul class="indexlink">'
 stub2='</ul></body></html>'
