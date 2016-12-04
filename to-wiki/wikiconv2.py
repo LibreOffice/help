de4f993364b2a996ca1282ef2b9efbbef1430681
Gabor Kelemen <kelemeng@gnome.hu>
2016-12-04 23:25:22 +0100

    Handle 'acronym' item type
    This was throwing an exception and not generating a wiki page
    Format with a simple italic text for now - better ideas welcome!
    
    Change-Id: I1b08c6b6dd2e96eef3cac4808fe23f911dc06cd3

--- to-wiki/wikiconv2.py
+++ to-wiki/wikiconv2.py
@@ -996,7 +996,8 @@ class Switch(SwitchInline):
 
 class Item(ElementBase):
     replace_type = \
-            {'start':{'code': '<code>',
+            {'start':{'acronym' : '\'\'',
+                      'code': '<code>',
                       'input': '<code>',
                       'keycode': '{{KeyCode|',
                       'tasto': '{{KeyCode|',
@@ -1008,7 +1009,8 @@ class Item(ElementBase):
                       'productname': '',
                       'unknown': '<code>'
                      },
-             'end':{'code': '</code>',
+             'end':{'acronym' : '\'\'',
+                    'code': '</code>',
                     'input': '</code>',
                     'keycode': '}}',
                     'tasto': '}}',
@@ -1020,7 +1022,8 @@ class Item(ElementBase):
                     'productname': '',
                     'unknown': '</code>'
                    },
-             'templ':{'code': False,
+             'templ':{'acronym': False,
+                      'code': False,
                       'input': False,
                       'keycode': True,
                       'tasto': True,
