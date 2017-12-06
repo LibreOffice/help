c09e0355995d329cbd0c60ff7e6008c0f06b45df
Shinnok <admin@shinnok.com>
2017-12-06 13:16:05 +0100

    Merge makelocal and makeonline into single script.
    Scripts were identical; only difference is whether we pass
    local yes/no to get_html.sh. Choice is now expressed via
    first param to makehelp.sh; build local is default.
    
    Change-Id: Icbe274fe6d9aeb86c7950aa69daf574e271e3b84
    Reviewed-on: https://gerrit.libreoffice.org/45943
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>
    Tested-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- help3xsl/makehelp.sh
+++ help3xsl/makehelp.sh
@@ -6,16 +6,27 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Arguments
+
+# $1 build local or webroot help [default=yes]
+# values: yes no
+
 productVersion=6.0
 workDir=$(realpath '../../workdir')
 langDirSource=$workDir'/HelpTranslatePartTarget'
+offline=$1
+
+if [ -z $offline ]
+then
+offline=yes
+fi
 
 ./get_hid2file.sh $productVersion $workDir
 
 for lang in $(ls $langDirSource)
 do
 ./get_bookmark.sh $lang $productVersion $workDir
-./get_html.sh $lang $productVersion $workDir 'no' '/'
+./get_html.sh $lang $productVersion $workDir $offline '/'
 ./get_tree.sh $lang $productVersion $workDir
 done
 
