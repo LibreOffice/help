2bd2f77c95426f575defe55dce68c58c290281b2
Olivier Hallot <olivier.hallot@libreoffice.org>
2018-06-24 23:02:18 -0300

    Fill help page for writer Style menu
    Fix XML
    
    Change-Id: I0d8d362774eecd200a6510dfecfe089274ae631d

--- AllLangHelp_swriter.mk
+++ AllLangHelp_swriter.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,swriter,\
     helpcontent2/source/text/swriter/00/00000404 \
     helpcontent2/source/text/swriter/00/00000405 \
     helpcontent2/source/text/swriter/00/00000406 \
+    helpcontent2/source/text/swriter/00/stylesmenu \
     helpcontent2/source/text/swriter/01/01120000 \
     helpcontent2/source/text/swriter/01/01150000 \
     helpcontent2/source/text/swriter/01/01160100 \
