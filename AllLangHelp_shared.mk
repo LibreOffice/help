1a36eb14ff3816bbaa3f3f3477b3c0a29cd73def
Jan Holesovsky <kendy@collabora.com>
2018-08-07 19:09:41 +0200

    Revert the .xhp parts of "Deploy videos in New Help (WIP)"
    Partially reverts commit 93ebf037016819613322ef1c39951a3de893ec14.
    
    Change-Id: I21224f97e9c47d5dfeb8e000102237a5664d3d41

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -517,7 +517,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
     helpcontent2/source/text/shared/05/00000160 \
     helpcontent2/source/text/shared/05/new_help \
     helpcontent2/source/text/shared/05/err_html \
-    helpcontent2/source/text/shared/06/youtubevideos \
     helpcontent2/source/text/shared/06/svx_screenshots \
     helpcontent2/source/text/shared/07/09000000 \
     helpcontent2/source/text/shared/3dsettings_toolbar \
