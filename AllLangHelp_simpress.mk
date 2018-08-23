0ea794ac79441e7a348832cb9ec1d94cdeed0341
Olivier Hallot <olivier.hallot@libreoffice.org>
2018-08-23 18:47:52 -0300

    Help page for Impress  Remote Connection dialog
    Change-Id: Ia3525f58f876fd92b447b99fd148fb12e6ac461b

--- AllLangHelp_simpress.mk
+++ AllLangHelp_simpress.mk
@@ -118,6 +118,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,simpress,\
     helpcontent2/source/text/simpress/01/effectoptionseffect \
     helpcontent2/source/text/simpress/01/effectoptionstext \
     helpcontent2/source/text/simpress/01/effectoptionstiming \
+    helpcontent2/source/text/simpress/01/remoteconnections \
     helpcontent2/source/text/simpress/01/slidesorter \
     helpcontent2/source/text/simpress/02/04010000 \
     helpcontent2/source/text/simpress/02/04020000 \
