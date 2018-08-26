a34ef69b30e0a489a22648346547dc696c9f4762
Andras Timar <andras.timar@collabora.com>
2018-08-26 21:50:46 +0200

    [cp] remove TDF Piwik tracker
    Change-Id: Icabdcae5d5828417d3540bf36f6bb747001189e3

--- help3xsl/help2.js
+++ help3xsl/help2.js
@@ -219,13 +219,6 @@ if (document.body.getElementsByTagName('meta')) {
     _paq.push(['disableCookies']);
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="//piwik.documentfoundation.org/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '68']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
     var system = getParameterByName("System");
 } else {
     var system = getSystem();
