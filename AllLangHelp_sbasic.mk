949606895b8d46fb97b0bfcda25cfb04f1f61e32
Olivier Hallot <olivier.hallot@libreoffice.org>
2018-11-27 22:10:50 -0200

    tdf#121736 initial Help pages for Python scripts
    Work in progress, initial addition of help pages for python
    scripts in LO.
    
    Change-Id: Iee95b1340c821fdb08524fdedeca3817b0de1459
    Reviewed-on: https://gerrit.libreoffice.org/64137
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -360,6 +360,9 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/main0601 \
     helpcontent2/source/text/sbasic/shared/special_vba_func \
     helpcontent2/source/text/sbasic/shared/vbasupport \
+    helpcontent2/source/text/sbasic/python/main0000 \
+    helpcontent2/source/text/sbasic/python/python_ide \
+    helpcontent2/source/text/sbasic/python/python_locations \
 ))
 
 # vim: set noet sw=4 ts=4:
