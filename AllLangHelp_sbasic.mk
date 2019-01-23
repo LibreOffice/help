dd752b3f23d2d2d7fc5d23457ae8d308cecf5346
Alain Romedenne <OpenOfficiant@sfr.fr>
2019-01-22 23:40:10 -0200

    Add more python examples
    Change-Id: Id34674cd96d615b8613a35e18d96886a3d39bae0

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -369,6 +369,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/python/python_examples \
     helpcontent2/source/text/sbasic/python/python_ide \
     helpcontent2/source/text/sbasic/python/python_locations \
+    helpcontent2/source/text/sbasic/python/python_platform \
     helpcontent2/source/text/sbasic/python/python_programming \
     helpcontent2/source/text/sbasic/python/python_screen \
     helpcontent2/source/text/sbasic/python/python_shell \
