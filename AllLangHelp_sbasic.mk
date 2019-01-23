6b4c81545da882fe38705999b6385f518801dce4
Alain Romedenne <pyoffice.romedenne@sfr.fr>
2019-01-23 19:02:00 -0200

    tdf#114263 Undocumented BASIC Options
    Option Compatible
    Option ClassModule
    
    Change-Id: Ieb07fddb216f4f8cc26089a30226da5760b0065e

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -356,6 +356,8 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/03170010 \
     helpcontent2/source/text/sbasic/shared/05060700 \
     helpcontent2/source/text/sbasic/shared/code-stubs \
+    helpcontent2/source/text/sbasic/shared/classmodule \
+    helpcontent2/source/text/sbasic/shared/compatible \
     helpcontent2/source/text/sbasic/shared/enum \
     helpcontent2/source/text/sbasic/shared/uno_objects \
     helpcontent2/source/text/sbasic/shared/stardesktop \
