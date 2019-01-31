ae3c8a0a8a17b5cd3c427d7c62a4832631e18bb2
Alain Romedenne <OpenOfficiant@sfr.fr>
2019-01-31 14:51:22 +0100

    Add Help page for Python session
    Change-Id: I01f1448176f5c95a5151bb3040e9c2ef53fcb7f1
    Reviewed-on: https://gerrit.libreoffice.org/67214
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -375,6 +375,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/python/python_platform \
     helpcontent2/source/text/sbasic/python/python_programming \
     helpcontent2/source/text/sbasic/python/python_screen \
+    helpcontent2/source/text/sbasic/python/python_session \
     helpcontent2/source/text/sbasic/python/python_shell \
 ))
 
