322db32b498e5a6fdfa8d7787aaa94b16731ee1b
LibreOﬃciant <LibreOfficiant@sfr.fr>
2019-04-17 18:19:51 +0200

    Monitoring document events with Python & Basic
    -Mutual help pages x-refs
    -tree addition
    -new bookmarks for
    --Advanced Basic Libraries
    --Appl. Progr. Interface
    
    NB: I wish I could create new </img> in gerrit
    
    OH:
    moved  new xhp to right position
    New image
    Update makefiles
    several x-ref fixes
    
    Change-Id: I97c92fc879df19f9e8de73e828cd3324ba2163fa

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -371,6 +371,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/vbasupport \
     helpcontent2/source/text/sbasic/python/main0000 \
     helpcontent2/source/text/sbasic/python/python_dialogs \
+    helpcontent2/source/text/sbasic/python/python_document_events \
     helpcontent2/source/text/sbasic/python/python_examples \
     helpcontent2/source/text/sbasic/python/python_ide \
     helpcontent2/source/text/sbasic/python/python_import \
