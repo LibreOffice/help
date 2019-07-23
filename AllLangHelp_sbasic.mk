16bdf1eeb1003a0bc17a80b94371feeab0facff8
LibreOﬃciant <LibreOfficiant@sfr.fr>
2019-07-23 17:16:46 +0200

    'Dialog Handler' new topic bunch of x-ref between pages make & toc
    Change-Id: I37e94fea48c394310298ff6f4b13f22c71c523c4

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -376,6 +376,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/python/python_dialogs \
     helpcontent2/source/text/sbasic/python/python_document_events \
     helpcontent2/source/text/sbasic/python/python_examples \
+    helpcontent2/source/text/sbasic/python/python_handler \
     helpcontent2/source/text/sbasic/python/python_ide \
     helpcontent2/source/text/sbasic/python/python_import \
     helpcontent2/source/text/sbasic/python/python_listener \
