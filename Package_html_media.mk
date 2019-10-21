0ff693fe6659c0ecd350f0e25624f6de2ceb4e39
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-10-21 20:15:54 -0300

    Add screenshot to Option load VBA Help page
    Change-Id: I35aab763f42ae1f4670f82900dd8ea25ca256f6e

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -335,6 +335,10 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/optviewpage/$(lang)/OptViewPage.png \
 	) \
+    screenshots/cui/ui/optfltrpage/OptFltrPage.png \
+    $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/cui/ui/optfltrpage/$(lang)/OptFltrPage.png \
+	) \
 ))
 
 # vim: set noet sw=4 ts=4:
