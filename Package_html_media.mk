60fed7a0a8fed52b397d70e9b2ccde313dffff98
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-10-24 15:09:14 -0300

    Add screenshot for Advanced Options page
    Use kde breeze theme
    
    Change-Id: If392c7da67395e599519baf875661997aa3ed26a

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -351,5 +351,9 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/optsavepage/$(lang)/OptSavePage.png \
 	) \
+    screenshots/cui/ui/optadvancedpage/OptAdvancedPage.png \
+    $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/cui/ui/optadvancedpage/$(lang)/OptAdvancedPage.png \
+	) \
 ))
 # vim: set noet sw=4 ts=4:
