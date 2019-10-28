4f5938e7cbe1511be74820d4c55a9a9a9640b89d
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-10-28 07:22:19 -0300

    Add screenhot in Font effects Help page
    Use kde5 breeze theme
    
    Change-Id: I3e8d7cc5358f4811c74af9675382a7acca9aa808
    Reviewed-on: https://gerrit.libreoffice.org/81595
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -367,5 +367,9 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/optsecuritypage/$(lang)/OptSecurityPage.png \
 	) \
+    screenshots/cui/ui/effectspage/EffectsPage.png \
+    $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/cui/ui/effectspage/$(lang)/EffectsPage.png \
+	) \
 ))
 # vim: set noet sw=4 ts=4:
