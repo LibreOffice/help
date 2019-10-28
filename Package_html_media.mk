f42a9a7b437fa3b7edda4499358ad2cf4fd4bb46
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-10-28 08:36:38 -0300

    Add screeshot for Hyperlink Doc page
    Use kde5 breeze theme
    
    Change-Id: I96b8e60f0ab5459c0c57ac0c187bb6f5e0840cd6
    Reviewed-on: https://gerrit.libreoffice.org/81601
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -371,5 +371,9 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/effectspage/$(lang)/EffectsPage.png \
 	) \
+    screenshots/cui/ui/hyperlinkdocpage/HyperlinkDocPage.png \
+    $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/cui/ui/hyperlinkdocpage/$(lang)/HyperlinkDocPage.png \
+	) \
 ))
 # vim: set noet sw=4 ts=4:
