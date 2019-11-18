3364b6db42899b82173030f26d58ca621b7f9a03
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-11-18 19:24:22 -0300

    Add PDF Export General Page screenshot
    Change-Id: I1f045faa224e8b24cc67a38989209217b15abdb1
    Reviewed-on: https://gerrit.libreoffice.org/83139
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -395,5 +395,9 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/optgeneralpage/$(lang)/OptGeneralPage.png \
 	) \
+    screenshots/filter/ui/pdfgeneralpage/PdfGeneralPage.png \
+    $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/filter/ui/pdfgeneralpage/$(lang)/PdfGeneralPage.png \
+	) \
 ))
 # vim: set noet sw=4 ts=4:
