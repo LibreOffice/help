207eca6f5d14f1b8a917f156c7905c39a4ba9761
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-12-29 18:20:44 -0300

    Add screenshot to slide design help page
    Change-Id: I106f1df1385863b008bfd7f9a4eb2594eb716319
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/85965
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -361,6 +361,10 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/modules/simpress/ui/presentationdialog/$(lang)/PresentationDialog.png \
 	) \
+    screenshots/modules/simpress/ui/slidedesigndialog/SlideDesignDialog.png \
+        $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
+		screenshots/modules/simpress/ui/slidedesigndialog/$(lang)/SlideDesignDialog.png \
+	) \
     screenshots/cui/ui/optviewpage/OptViewPage.png \
     $(foreach lang, $(filter-out en-US,$(gb_HELP_LANGS)),\
 		screenshots/cui/ui/optviewpage/$(lang)/OptViewPage.png \
