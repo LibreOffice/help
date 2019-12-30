363bc1ec76f31829614a103213b0fac3174599c1
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-12-30 11:13:55 -0300

    Add animation timing screenhot to help page
    Change-Id: Ieb94d4b05ad2a1105cf50731ec870f1a796a7ea5
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/86009
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangPackage_html_media_lang.mk
+++ AllLangPackage_html_media_lang.mk
@@ -256,6 +256,7 @@ $(foreach lang,$(filter-out en-US,$(gb_HELP_LANGS)),$(eval $(call gb_AllLangPack
     screenshots/modules/simpress/ui/definecustomslideshow/$(lang)/DefineCustomSlideShow.png \
     screenshots/modules/simpress/ui/customanimationeffecttab/$(lang)/EffectTab.png \
     screenshots/modules/simpress/ui/customanimationtexttab/$(lang)/TextAnimationTab.png \
+    screenshots/modules/simpress/ui/customanimationtimingtab/$(lang)/TimingTab.png \
     screenshots/cui/ui/optviewpage/$(lang)/OptViewPage.png \
     screenshots/cui/ui/optfltrpage/$(lang)/OptFltrPage.png \
     screenshots/cui/ui/opthtmlpage/$(lang)/OptHtmlPage.png \
