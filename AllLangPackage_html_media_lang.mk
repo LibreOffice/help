e2303a462f22daec2d7b5a1faa5cdf6720c52364
Olivier Hallot <olivier.hallot@libreoffice.org>
2019-12-30 00:52:38 -0300

    Add define custom presentation dialog screenshot
    Change-Id: I23462439f2cc3d572fb0f352812fb416901ca4ff
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/85982
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangPackage_html_media_lang.mk
+++ AllLangPackage_html_media_lang.mk
@@ -253,6 +253,7 @@ $(foreach lang,$(filter-out en-US,$(gb_HELP_LANGS)),$(eval $(call gb_AllLangPack
     screenshots/modules/simpress/ui/headerfooterdialog/$(lang)/HeaderFooterDialog.png \
     screenshots/modules/simpress/ui/masterlayoutdlg/$(lang)/MasterLayoutDialog.png \
     screenshots/modules/simpress/ui/customslideshows/$(lang)/CustomSlideShows.png \
+    screenshots/modules/simpress/ui/definecustomslideshow/$(lang)/DefineCustomSlideShow.png \
     screenshots/cui/ui/optviewpage/$(lang)/OptViewPage.png \
     screenshots/cui/ui/optfltrpage/$(lang)/OptFltrPage.png \
     screenshots/cui/ui/opthtmlpage/$(lang)/OptHtmlPage.png \
