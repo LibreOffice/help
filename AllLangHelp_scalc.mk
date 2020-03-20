562631aa3b0f0a6055e899e8b36491bee52fcb73
Olivier Hallot <olivier.hallot@libreoffice.org>
2020-03-20 11:01:08 -0300

    Review, refactor Calc Data Menu Help pages
    - update to latest menu entries
    - embed headings from individual Help files
    - WIP: Statistics menu entry will come later
    
    Change-Id: I66a2627b7bea65945210b6dcc2d1c3ed8783aec9
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/90796
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -180,6 +180,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/12120100 \
     helpcontent2/source/text/scalc/01/12120200 \
     helpcontent2/source/text/scalc/01/12120300 \
+    helpcontent2/source/text/scalc/01/calculate \
     helpcontent2/source/text/scalc/01/data_form \
     helpcontent2/source/text/scalc/01/data_provider \
     helpcontent2/source/text/scalc/01/ex_data_stat_func \
