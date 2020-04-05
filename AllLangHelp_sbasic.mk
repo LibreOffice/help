7495ecc3f5185ecf23461b9949be87daaf99e746
LibreOﬃciant <LibreOfficiant@sfr.fr>
2020-04-05 13:28:45 +0100

    tdf#129366 Resume statement new help page
    its 3 available options are:
    - 0
    - label
    - Next
    
    + associated examples in combination with On Error statement
    
    Change-Id: I0986816580835bf9b2b2657ecdbdc06118b1ff9d

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -372,6 +372,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/keys \
     helpcontent2/source/text/sbasic/shared/main0211 \
     helpcontent2/source/text/sbasic/shared/main0601 \
+    helpcontent2/source/text/sbasic/shared/Resume \
     helpcontent2/source/text/sbasic/shared/special_vba_func \
     helpcontent2/source/text/sbasic/shared/vbasupport \
     helpcontent2/source/text/sbasic/python/main0000 \
