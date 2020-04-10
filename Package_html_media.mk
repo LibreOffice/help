540bcbb9a74671246975ece908a8bc9c34d1da9b
LibreOﬃciant <LibreOfficiant@sfr.fr>
2020-04-10 11:03:49 +0100

    3 Basic syntax diagrams
    - For statement variations
    - On Error
    
    Change-Id: Ic4b25b9d44adeb0c80fc2e24cc60301b9325c1a3
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/92010
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -108,7 +108,10 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     helpimg/smzb8.png \
     helpimg/smzb9.png \
     helpimg/sbasic/Call_statement.svg \
+    helpimg/sbasic/For-Each_statement.svg \
+    helpimg/sbasic/For-Next_statement.svg \
     helpimg/sbasic/LetSet_statement.svg \
+    helpimg/sbasic/On-Error_statement.svg \
     helpimg/sbasic/Resume_statement.svg \
     helpimg/scalc/coordinates-to-polar-01.svg \
     helpimg/starmath/harpoon.svg \
