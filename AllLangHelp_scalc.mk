cdda03a5a4fdf1fb980f6973b8b31e65560a3b07
Olivier Hallot <olivier.hallot@libreoffice.org>
2020-05-22 14:25:26 -0300

    tdf81599 Better Help pages for Calc Solvers
    Add information on algorithms paramenters
    
    Change-Id: I36f330593687efa616594e397cb7d6054c2198ed

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -266,6 +266,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/live_data_stream \
     helpcontent2/source/text/scalc/01/solver \
     helpcontent2/source/text/scalc/01/solver_options \
+    helpcontent2/source/text/scalc/01/solver_options_algo \
     helpcontent2/source/text/scalc/01/statistics \
     helpcontent2/source/text/scalc/01/statistics_anova \
     helpcontent2/source/text/scalc/01/statistics_correlation \
