fcf75206c5f8f59ac464dc3b9b9ca869ce3a7bd5
Alain Romedenne <LibreOfficiant@sfr.fr>
2020-06-17 08:32:31 +0200

    Basic compiler/runtime options aggregation
    - Base
    - Explicit
    - ClassModule
    - Compatible
    - Private Module
    - VBASupport
    
    Change-Id: If5edfe93a744d847bc387d868cbb26292b60ea0a
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/96493
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -362,6 +362,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/classmodule \
     helpcontent2/source/text/sbasic/shared/compatible \
     helpcontent2/source/text/sbasic/shared/compatibilitymode \
+    helpcontent2/source/text/sbasic/shared/Compiler_options \
     helpcontent2/source/text/sbasic/shared/enum \
     helpcontent2/source/text/sbasic/shared/ErrVBA \
     helpcontent2/source/text/sbasic/shared/fragments \
