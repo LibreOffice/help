5bcca636e88a2fb01435b22c8134fc621b0be16e
Olivier Hallot <olivier.hallot@libreoffice.org>
2020-06-16 23:57:06 -0300

    tdf#134032 Calling Calc function from basic
    Change-Id: I7c3c64e0df4d89b46e49afb869f3e2de0825a6e2
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/96485
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>
    (cherry picked from commit 7af288d0fa90e5e31c29014501f06f776a00366a)

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -358,6 +358,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/03170010 \
     helpcontent2/source/text/sbasic/shared/05060700 \
     helpcontent2/source/text/sbasic/shared/code-stubs \
+    helpcontent2/source/text/sbasic/shared/calc_functions \
     helpcontent2/source/text/sbasic/shared/classmodule \
     helpcontent2/source/text/sbasic/shared/compatible \
     helpcontent2/source/text/sbasic/shared/compatibilitymode \
