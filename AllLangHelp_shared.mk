ab4b270d35eb6db2f0e2408f33e7817a9a0e7ce0
Olivier Hallot <olivier.hallot@libreoffice.org>
2020-09-22 21:23:11 -0300

    Refactor Relations Help page
    Change-Id: Iaf32149f3a546c04bc13206213c748bc6be01b4c
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/103222
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -860,7 +860,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
 ))
 
 $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
-    helpcontent2/source/text/shared/explorer/database/02010101 \
     helpcontent2/source/text/shared/explorer/database/04000000 \
     helpcontent2/source/text/shared/explorer/database/04030000 \
     helpcontent2/source/text/shared/explorer/database/05000000 \
