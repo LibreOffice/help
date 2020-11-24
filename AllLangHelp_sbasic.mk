f908da1aac8b1b5d95435ec9f1ae5cf1367e9004
Alain Romedenne <LibreOfficiant@sfr.fr>
2020-11-24 16:33:22 +0100

    ScriptForge library modules - WiP
    + Array module
    + Dictionary module
    
    Change-Id: Ida2290005befb59f2b7c3699744ff49b32226e24

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -77,6 +77,8 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/03/lib_importwiz \
     helpcontent2/source/text/sbasic/shared/03/lib_script \
     helpcontent2/source/text/sbasic/shared/03/lib_ScriptForge \
+    helpcontent2/source/text/sbasic/shared/03/sf_array \
+    helpcontent2/source/text/sbasic/shared/03/sf_dictionary \
     helpcontent2/source/text/sbasic/shared/03/lib_schedule \
     helpcontent2/source/text/sbasic/shared/03/lib_template \
     helpcontent2/source/text/sbasic/shared/03/lib_wikieditor \
