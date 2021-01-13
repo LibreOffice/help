d059ecef637c037e348a713ac8587aed8a0e738a
Balaharipreetha Muthu <balaharipreetha@gmail.com>
2021-01-13 19:25:55 +0100

    tdf#139436 Add Help page for Format - Conditional - Data bar - More Options dialog
    Change-Id: I83faa2d1edb8f5cba08a7b1054bd66fc17f835b9

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -184,6 +184,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/cell_styles \
     helpcontent2/source/text/scalc/01/data_form \
     helpcontent2/source/text/scalc/01/data_provider \
+    helpcontent2/source/text/scalc/01/databar_more_options \
     helpcontent2/source/text/scalc/01/ex_data_stat_func \
     helpcontent2/source/text/scalc/01/format_graphic \
     helpcontent2/source/text/scalc/01/ful_func \
