5fd683cd1c13cc20d0c7205c895b4e5ca1c39196
Olivier Hallot <olivier.hallot@libreoffice.org>
2021-01-31 14:10:55 -0300

    Refactor some database pages
    Place in sdatabase/ folder
    
    Change-Id: Ia787f7634755be2fd4936914cacecded5bee4221
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/110208
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -861,8 +861,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
 ))
 
 $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
-    helpcontent2/source/text/shared/explorer/database/05010000 \
-    helpcontent2/source/text/shared/explorer/database/05010100 \
     helpcontent2/source/text/shared/explorer/database/05020000 \
     helpcontent2/source/text/shared/explorer/database/05020100 \
     helpcontent2/source/text/shared/explorer/database/05030000 \
