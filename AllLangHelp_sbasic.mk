ae361f8f4f70c43d7082dd37c0a3f69bf49fa3e0
Rafael Lima <rafael.palma.lima@gmail.com>
2021-03-10 15:46:49 +0100

    Create SF_Database Help page
    Change-Id: I4ab158e8089973af24155d3b9d0697734acdf4bb
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/112228
    Tested-by: Jenkins
    Tested-by: Jean-Pierre Ledure <jp@ledure.be>
    Reviewed-by: Jean-Pierre Ledure <jp@ledure.be>

--- AllLangHelp_sbasic.mk
+++ AllLangHelp_sbasic.mk
@@ -79,6 +79,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,sbasic,\
     helpcontent2/source/text/sbasic/shared/03/lib_ScriptForge \
     helpcontent2/source/text/sbasic/shared/03/sf_array \
     helpcontent2/source/text/sbasic/shared/03/sf_calc \
+    helpcontent2/source/text/sbasic/shared/03/sf_database \
     helpcontent2/source/text/sbasic/shared/03/sf_dialog \
     helpcontent2/source/text/sbasic/shared/03/sf_dialogcontrol \
     helpcontent2/source/text/sbasic/shared/03/sf_dictionary \
