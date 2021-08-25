7d86aa20fd836dd1b94dbe50d2ffb4b203ea8c1e
Olivier Hallot <olivier.hallot@libreoffice.org>
2021-08-25 10:45:13 -0300

    tdf#137084 Update writer View menu
    part A - View of comment#18
    
    Change-Id: I8ddfdc8799f499d4730b08e217b07baaa3799172
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/121036
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_swriter.mk
+++ AllLangHelp_swriter.mk
@@ -200,6 +200,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,swriter,\
     helpcontent2/source/text/swriter/01/mm_seltab \
     helpcontent2/source/text/swriter/01/outlinecontent_visibility \
     helpcontent2/source/text/swriter/01/selection_mode \
+    helpcontent2/source/text/swriter/01/show_whitespace \
     helpcontent2/source/text/swriter/01/title_page \
     helpcontent2/source/text/swriter/01/watermark \
     helpcontent2/source/text/swriter/02/02110000 \
