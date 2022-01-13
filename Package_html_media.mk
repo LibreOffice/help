eb1bf86ac137d050daaaed114a70c29002acc4fe
Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
2022-01-13 16:25:47 +0200

    tdf#123318 Add navigation menu entry LibreOffice for Shared
    Change-Id: Ibd46738e03721127e07ee1575e0a925fc36fbe9d
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/128382
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- Package_html_media.mk
+++ Package_html_media.mk
@@ -378,6 +378,7 @@ $(eval $(call gb_Package_add_files_with_dir,helpcontent2_html_media,$(LIBO_SHARE
     navigation/libo-draw.svg \
     navigation/libo-impress.svg \
     navigation/libo-math.svg \
+    navigation/libo-symbol-black.svg \
     navigation/libo-symbol-white.svg \
     navigation/libo-writer.svg \
     screenshots/modules/scalc/ui/deletecells/DeleteCellsDialog.png \
