8219f0b1d741dc3e3927a1fb9ee4fc0b916d57e1
Olivier Hallot <olivier.hallot@libreoffice.org>
2022-04-08 11:18:09 -0300

    tdf#102265 Add Format - List submenu for Impress
    + usual refactoring and icon size adjustments
    
    Change-Id: I614438a36fa1ca34ddfaac81aadb0f54bd51940e
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/132736
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_simpress.mk
+++ AllLangHelp_simpress.mk
@@ -198,6 +198,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,simpress,\
     helpcontent2/source/text/simpress/main0103 \
     helpcontent2/source/text/simpress/main0104 \
     helpcontent2/source/text/simpress/main_format \
+    helpcontent2/source/text/simpress/format_submenu_lists \
     helpcontent2/source/text/simpress/main_tools \
     helpcontent2/source/text/simpress/main0107 \
     helpcontent2/source/text/simpress/main0113 \
