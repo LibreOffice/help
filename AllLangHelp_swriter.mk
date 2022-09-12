6ce0e3da2dd19a12370bacac5aa84ed61a446f08
Miklos Vajna <vmiklos@collabora.com>
2022-09-12 15:55:49 +0200

    Add initial help for the sw contentcontrollistitemdlg
    Document the relation between display names and values.
    
    Change-Id: I559d7ae12514797d8b394c6c9f3a5608a9403079
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/139801
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- AllLangHelp_swriter.mk
+++ AllLangHelp_swriter.mk
@@ -207,6 +207,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,swriter,\
     helpcontent2/source/text/swriter/01/title_page \
     helpcontent2/source/text/swriter/01/watermark \
     helpcontent2/source/text/swriter/01/contentcontrol00 \
+    helpcontent2/source/text/swriter/01/contentcontrol01 \
     helpcontent2/source/text/swriter/02/02110000 \
     helpcontent2/source/text/swriter/02/03210000 \
     helpcontent2/source/text/swriter/02/03220000 \
