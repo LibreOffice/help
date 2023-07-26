90b7aad666d0338c819b375f574cdbeb74d47037
Olivier Hallot <olivier.hallot@libreoffice.org>
2023-07-26 08:55:36 -0300

    tdf#105122 entry page for doc themes.
    Work in progress
    
    This patch adds 2 help pages for the document themes dialog and document theme color dialog.
    
    Contents will be added later.
    
    Change-Id: Ia357a30e6c749898a3a636eba922bd1629c3249f
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/154941
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -380,6 +380,8 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
     helpcontent2/source/text/shared/01/signexistingpdf \
     helpcontent2/source/text/shared/01/securitywarning \
     helpcontent2/source/text/shared/01/selectcertificate \
+    helpcontent2/source/text/shared/01/themescolordialog \
+    helpcontent2/source/text/shared/01/themesdialog \
     helpcontent2/source/text/shared/01/timestampauth \
     helpcontent2/source/text/shared/guide/tipoftheday \
     helpcontent2/source/text/shared/01/webhtml \
