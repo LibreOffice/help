ad8d101fec688539999cab824616f990a2cb0293
Olivier Hallot <olivier.hallot@libreoffice.org>
2024-01-22 09:47:12 -0300

    tdf#155875 UI cmds Format Text (cont)
    + Small Capitals
    + Patch in advance of fixing bugs tdf#149409 and tdf#157358
    + refactoring
    
    Change-Id: Ibfc5bc8d0d9a3474914950232362ef94d2804880
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/162397
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -419,6 +419,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
     helpcontent2/source/text/shared/01/search_commands \
     helpcontent2/source/text/shared/01/Shrink \
     helpcontent2/source/text/shared/01/SignaturesMenu \
+    helpcontent2/source/text/shared/01/SmallCaps \
     helpcontent2/source/text/shared/01/SpellOnline \
     helpcontent2/source/text/shared/01/select_template_category \
     helpcontent2/source/text/shared/01/signexistingpdf \
