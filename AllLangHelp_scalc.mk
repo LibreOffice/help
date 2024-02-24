cb24d887c855c8e8a1a9c2c31789b25091045e2d
Olivier Hallot <olivier.hallot@libreoffice.org>
2024-02-24 18:11:39 -0300

    tdf#159872 Prepare Help page for RANDARRAY function
    Contents will be added upon tdf#126573
    
    Change-Id: I54fed6b04b87f1dd5d9d7f344c022dd31a57b680
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/163894
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -260,6 +260,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/func_opt_prob_hit \
     helpcontent2/source/text/scalc/01/func_opt_prob_inmoney \
     helpcontent2/source/text/scalc/01/func_opt_touch \
+    helpcontent2/source/text/scalc/01/func_randarray \
     helpcontent2/source/text/scalc/01/func_rawsubtract \
     helpcontent2/source/text/scalc/01/func_regex \
     helpcontent2/source/text/scalc/01/func_replaceb \
