772dac9c72752c82c015bcd222655b50a6eeb9eb
Olivier Hallot <olivier.hallot@libreoffice.org>
2024-03-25 11:59:22 -0300

    tdf#160035 Remove extra Help page
    Page 19040000.xhp was not target of nay other link
    
    Change-Id: I2b3311aaa8f6bd2616096e922b3118856c749bfe
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/165289
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_swriter.mk
+++ AllLangHelp_swriter.mk
@@ -270,7 +270,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,swriter,\
     helpcontent2/source/text/swriter/02/19010000 \
     helpcontent2/source/text/swriter/02/19020000 \
     helpcontent2/source/text/swriter/02/19030000 \
-    helpcontent2/source/text/swriter/02/19040000 \
     helpcontent2/source/text/swriter/02/19050000 \
     helpcontent2/source/text/swriter/02/add_to_list \
     helpcontent2/source/text/swriter/02/word_count_stb \
