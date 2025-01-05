4c5a09f2ad9c2396860dc89553e4a6b31f2e0896
Olivier Hallot <olivier.hallot@libreoffice.org>
2025-01-05 19:37:31 -0300

    tdf#137419 Remove Data Provider help page
    Change-Id: I898f9165a27b009a379a1c3d59886981b6f9f2d5
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/179810
    Tested-by: Jenkins
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- AllLangHelp_scalc.mk
+++ AllLangHelp_scalc.mk
@@ -181,7 +181,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,scalc,\
     helpcontent2/source/text/scalc/01/cell_styles \
     helpcontent2/source/text/scalc/01/calculation_accuracy \
     helpcontent2/source/text/scalc/01/data_form \
-    helpcontent2/source/text/scalc/01/data_provider \
     helpcontent2/source/text/scalc/01/databar_more_options \
     helpcontent2/source/text/scalc/01/database_table_reference \
     helpcontent2/source/text/scalc/01/default_number_formats \
