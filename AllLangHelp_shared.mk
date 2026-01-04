b9c56ad672ef100b9bb6db3a1e3cf4d5c9face8e
Olivier Hallot <olivier.hallot@libreoffice.org>
2026-01-04 11:56:23 -0300

    Move baseline grid display page to Writer folder
    baseline grid is Writer-exclusive
    
    Remove some unused code
    
    Change-Id: Ib800d5ecbdf79f045d18e0dccae71ad3490fc808
    Reviewed-on: https://gerrit.libreoffice.org/c/help/+/196521
    Tested-by: Jenkins
    Reviewed-by: Olivier Hallot <olivier.hallot@libreoffice.org>

--- AllLangHelp_shared.mk
+++ AllLangHelp_shared.mk
@@ -593,7 +593,6 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,shared,\
     helpcontent2/source/text/shared/02/24080000 \
     helpcontent2/source/text/shared/02/24090000 \
     helpcontent2/source/text/shared/02/24100000 \
-    helpcontent2/source/text/shared/02/baseline_grid \
     helpcontent2/source/text/shared/02/basicshapes \
     helpcontent2/source/text/shared/02/blockarrows \
     helpcontent2/source/text/shared/02/callouts \
--- AllLangHelp_swriter.mk
+++ AllLangHelp_swriter.mk
@@ -163,6 +163,7 @@ $(eval $(call gb_AllLangHelp_add_helpfiles,swriter,\
     helpcontent2/source/text/swriter/01/06210000 \
     helpcontent2/source/text/swriter/01/06220000 \
     helpcontent2/source/text/swriter/01/06990000 \
+    helpcontent2/source/text/swriter/01/baseline_grid \
     helpcontent2/source/text/swriter/01/paragraph_hyphenation \
     helpcontent2/source/text/swriter/01/contentcontrol00 \
     helpcontent2/source/text/swriter/01/copyfielddialog \
